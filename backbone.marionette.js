fe89b264e295e0e689f0a07a01166bf0645beb86
Derick Bailey <derickbailey@gmail.com>
2012-01-19 22:27:46 -0600

    bump version to v0.2.5

--- backbone.marionette.js
+++ backbone.marionette.js
@@ -1,4 +1,4 @@
-// Backbone.Marionette v0.2.4
+// Backbone.Marionette v0.2.5
 //
 // Copyright (C)2011 Derick Bailey, Muted Solutions, LLC
 // Distributed Under MIT License
@@ -9,7 +9,7 @@
 Backbone.Marionette = (function(Backbone, _, $){
   var Marionette = {};
 
-  Marionette.version = "0.2.4";
+  Marionette.version = "0.2.5";
 
   // Region Manager
   // --------------

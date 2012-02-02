3aa02c3a1e10beac4c0153ab4133f8196acebae0
Jonathan Mahoney <jonathan@mahoney.eu>
2012-02-02 05:33:52 +0000

    Minor spec amendments

--- spec/javascripts/eventAggregator.spec.js
+++ spec/javascripts/eventAggregator.spec.js
@@ -11,13 +11,18 @@ describe("event aggregator", function(){
   describe("when triggering events with the event aggregator", function() {
     var MyApp = new Backbone.Marionette.Application();
 
-    it ("should throw a descriptive error if there is no callback for the trigger", function(){
+    it ("should throw a descriptive error if there is no callback for the triggered event", function(){
       var eventErrorMessage = function (event) {
         return 'Attempted to trigger application event \'' + event + '\', however there is no associated callback.';
       };
-
+      
+      // Trigger an event when there are no callbacks present at all
       expect(function () { MyApp.vent.trigger('save:image'); }).toThrow(new Error(eventErrorMessage('save:image')));
-      expect(function () { MyApp.vent.trigger('') }).toThrow(new Error(eventErrorMessage('')));
+      expect(function () { MyApp.vent.trigger(); }).toThrow(new Error(eventErrorMessage('undefined')));
+      
+      // Trigger an event when there is at least one callback, but the name is not matched
+      MyApp.vent.bind('new:file', function () { });
+      expect(function () { MyApp.vent.trigger('new:files') }).toThrow(new Error(eventErrorMessage('new:files')));
     });
   });
 

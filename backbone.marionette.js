6670637458828c0c136b5235bdb22d5126fd749b
Derick Bailey <derickbailey@gmail.com>
2012-03-25 14:27:37 -0500

    bumped version to v0.7.0, and made the failing test message more detailed to remind me what i need to do

--- backbone.marionette.js
+++ backbone.marionette.js
@@ -1,4 +1,4 @@
-// Backbone.Marionette v0.6.4
+// Backbone.Marionette v0.7.0
 //
 // Copyright (C)2011 Derick Bailey, Muted Solutions, LLC
 // Distributed Under MIT License
@@ -9,7 +9,7 @@
 Backbone.Marionette = (function(Backbone, _, $){
   var Marionette = {};
 
-  Marionette.version = "0.6.4";
+  Marionette.version = "0.7.0";
 
   // Marionette.View
   // ---------------

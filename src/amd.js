23540318a5bdadf97800b692cd391116bf7a959c
Derick Bailey <derickbailey@gmail.com>
2012-04-19 09:00:50 -0500

    rebuild with better amd support, using new version of anvil

--- src/amd.js
+++ src/amd.js
@@ -1,9 +1,6 @@
 (function (root, factory) {
   if (typeof exports === 'object') {
 
-    // Node. Does not work with strict CommonJS, but
-    // only CommonJS-like enviroments that support module.exports,
-    // like Node.
     var jquery = require('jquery');
     var underscore = require('underscore');
     var backbone = require('backbone');
@@ -12,11 +9,12 @@
 
   } else if (typeof define === 'function' && define.amd) {
 
-    // AMD. Register as an anonymous module.
     define(['jquery', 'underscore', 'backbone'], factory);
 
   } 
 }(this, function ($, _, Backbone) {
+
   // import "backbone.marionette.js"
   return Backbone.Marionette; 
+
 }));

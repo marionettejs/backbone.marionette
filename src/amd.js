ca1316183bb50cf7122e35bcbb671b7f9cafa173
Derick Bailey <derickbailey@gmail.com>
2012-04-19 08:18:36 -0500

    updated amd wrapper to support node and requirejs

--- src/amd.js
+++ src/amd.js
@@ -1,5 +1,22 @@
-define(['jquery', 'underscore', 'backbone'], 
-function($, _, Backbone){
+(function (root, factory) {
+  if (typeof exports === 'object') {
+
+    // Node. Does not work with strict CommonJS, but
+    // only CommonJS-like enviroments that support module.exports,
+    // like Node.
+    var jquery = require('jquery');
+    var underscore = require('underscore');
+    var backbone = require('backbone');
+
+    module.exports = factory(jquery, underscore, backbone);
+
+  } else if (typeof define === 'function' && define.amd) {
+
+    // AMD. Register as an anonymous module.
+    define(['jquery', 'underscore', 'backbone'], factory);
+
+  } 
+}(this, function ($, _, Backbone) {
   // import "backbone.marionette.js"
   return Backbone.Marionette; 
-});
+}));

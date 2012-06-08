ead81586931dd0deed4237c4c1d3f9599270848d
Derick Bailey <derickbailey@gmail.com>
2012-06-08 08:38:52 -0500

    reverted itemview back to previous code without separate renderer

--- src/backbone.marionette.itemview.js
+++ src/backbone.marionette.itemview.js
@@ -4,107 +4,71 @@
 // A single item view implementation that contains code for rendering
 // with underscore.js templates, serializing the view's model or collection,
 // and calling several methods on extended views, such as `onRender`.
-Marionette.ItemView = (function(Mariontte, _){
+Marionette.ItemView =  Marionette.View.extend({
+  constructor: function(){
+    Marionette.View.prototype.constructor.apply(this, arguments);
+    this.initialEvents();
+  },
 
-  // **ItemView**
-  var ItemView = Marionette.View.extend({
-    constructor: function(){
-      Marionette.View.prototype.constructor.apply(this, arguments);
-      this.initialEvents();
-
-      this.renderer = new ItemViewRendering(this);
-    },
-
-    // Configured the initial events that the item view 
-    // binds to. Override this method to prevent the initial
-    // events, or to add your own initial events.
-    initialEvents: function(){
-      if (this.collection){
-        this.bindTo(this.collection, "reset", this.render, this);
-      }
-    },
-
-    // Render the view, defaulting to underscore.js templates.
-    // You can override this in your view definition.
-    render: function(){
-      return this.renderer.render();
-    },
-
-    // Override the default close event to add a few
-    // more events that are triggered.
-    close: function(){
-      this.trigger('item:before:close');
-      Marionette.View.prototype.close.apply(this, arguments);
-      this.trigger('item:closed');
+  // Configured the initial events that the item view 
+  // binds to. Override this method to prevent the initial
+  // events, or to add your own initial events.
+  initialEvents: function(){
+    if (this.collection){
+      this.bindTo(this.collection, "reset", this.render, this);
     }
-  });
+  },
 
-  // **ItemViewRendering**
-  // Object to manage rendering itemView. Extracted to reduce the
-  // overhead and memory usage of anonymous / callback functions.
-  var ItemViewRendering = function(view){
-      this.view = view;
-      _.bindAll(this);
-  };
+  // Render the view, defaulting to underscore.js templates.
+  // You can override this in your view definition.
+  render: function(){
+    var that = this;
 
-  _.extend(ItemViewRendering.prototype, {
+    var deferredRender = $.Deferred();
 
-    // Run the rendering process, call `beforeRender` on the view
-    render: function(){
-      this.deferredRender = $.Deferred();
-      callDeferredMethod(this.view.beforeRender, this.serializeData, this.view);
-      return this.deferredRender;
-    },
+    var beforeRenderDone = function() {
+      that.trigger("before:render", that);
+      that.trigger("item:before:render", that);
 
-    // Called after the `beforeRender` method has been run. Triggers
-    // events on teh view, and serializes data
-    serializeData: function() {
-      var that = this;
+      var deferredData = that.serializeData();
+      $.when(deferredData).then(dataSerialized);
+    } 
 
-      this.view.trigger("before:render", this.view);
-      this.view.trigger("item:before:render", this.view);
-
-      var serializeData = this.view.serializeData()
-      $.when(serializeData).then(function(data){
-        that.renderTemplateAndData(data);
-      });
-    },
-
-    // Called after data serialization has finished. Runs the
-    // actual render process for the template and data
-    renderTemplateAndData: function(data){
-      var that = this;
-      var renderHtml = this.renderHtml(data);
-
-      $.when(renderHtml).then(function(html){
-        that.templateRendered(html);
-      });
-    },
-    
-    // Render the data for this item view in to some HTML.
-    // Override this method to replace the specific way in
-    // which an item view has it's data rendered in to html.
-    renderHtml: function(data) {
-      var template = this.view.getTemplate();
-      return Marionette.Renderer.render(template, data);
-    },
-
-    // Runs after the template has been rendered. Calls the
-    // `onRender` method on the view.
-    templateRendered: function(html){
-      this.view.$el.html(html);
-      callDeferredMethod(this.view.onRender, this.renderDone, this.view);
-    },
-
-    // Runs after the `onRender` method, triggers some events on
-    // the view and resolves the deferred render promise.
-    renderDone: function(){
-      this.view.trigger("render", this.view);
-      this.view.trigger("item:rendered", this.view);
-      this.deferredRender.resolve();
+    var dataSerialized = function(data){
+      var asyncRender = that.renderHtml(data);
+      $.when(asyncRender).then(templateRendered);
     }
-  });
 
-  return ItemView;
-})(Marionette, _);
+    var templateRendered = function(html){
+      that.$el.html(html);
+      callDeferredMethod(that.onRender, onRenderDone, that);
+    }
 
+    var onRenderDone = function(){
+      that.trigger("render", that);
+      that.trigger("item:rendered", that);
+
+      deferredRender.resolve();
+    }
+
+    callDeferredMethod(this.beforeRender, beforeRenderDone, this);
+
+    return deferredRender.promise();
+  },
+
+  // Render the data for this item view in to some HTML.
+  // Override this method to replace the specific way in
+  // which an item view has it's data rendered in to html.
+  renderHtml: function(data) {
+    var template = this.getTemplate();
+    return Marionette.Renderer.render(template, data);
+  },
+
+  // Override the default close event to add a few
+  // more events that are triggered.
+  close: function(){
+    this.trigger('item:before:close');
+    Marionette.View.prototype.close.apply(this, arguments);
+    this.trigger('item:closed');
+  }
+});

fd7101baf422b7ec64d64e074006d23843888d95
Derick Bailey <derickbailey@gmail.com>
2012-06-16 19:07:17 -0500

    failing spec for removing last item in collection, showing emptyView

--- spec/javascripts/collectionView.spec.js
+++ spec/javascripts/collectionView.spec.js
@@ -254,6 +254,30 @@ describe("collection view", function(){
       });
     });
 
+    describe("when the last item is removed from a collection", function(){
+      var collectionView, closeSpy;
+
+      beforeEach(function(){
+        var collection = new Collection([{foo: "wut"}]);
+
+        collectionView = new EmptyCollectionView({
+          collection: collection
+        });
+
+        collectionView.render();
+
+        collection.remove(collection.at(0));
+      });
+
+      it("should append the html for the emptyView", function(){
+        expect($(collectionView.$el)).toHaveHtml("<span class=\"isempty\"></span>");
+      });
+
+      it("should reference each of the rendered view items", function(){
+        expect(_.size(collectionView.children)).toBe(1);
+      });
+    });
+
   });
 
   describe("when a collection is reset after the view is loaded", function(){

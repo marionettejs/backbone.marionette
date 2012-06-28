fb3ccc7a976730e094a16b4c00c2c2c11c47be8a
Derick Bailey <derickbailey@gmail.com>
2012-06-27 20:48:46 -0500

    call closeEmptyView on showColletion to avoid incorrect state of showing empty view flag

--- spec/javascripts/async/async.collectionView.spec.js
+++ spec/javascripts/async/async.collectionView.spec.js
@@ -208,6 +208,35 @@ describe("async collection view", function(){
         });
       });
 
+      describe("when the emptyView has been rendered for an empty collection and then collection reset, recieving some values. Then adding an item to the collection", function () {
+
+          var collectionView, closeSpy;
+
+          beforeEach(function () {
+              var collection = new Collection();
+              collectionView = new EmptyCollectionView({
+                  collection: collection
+              });
+
+              collectionView.render();
+
+              closeSpy = spyOn(EmptyView.prototype, "close");
+              closeSpy.andCallThrough();
+
+              collection.reset([{ foo: "bar" }, { foo: "baz"}]);
+              
+              collection.add({ foo: "wut" });
+          });
+
+          it("should close the emptyView", function () {
+              expect(closeSpy).toHaveBeenCalled();
+          });
+
+          it("should show all three items without empty view", function () {
+              expect($(collectionView.$el)).toHaveHtml("<span>bar</span><span>baz</span><span>wut</span>");
+          });
+      });
+
       describe("when the last item is removed from a collection", function(){
         var collectionView, closeSpy;
 

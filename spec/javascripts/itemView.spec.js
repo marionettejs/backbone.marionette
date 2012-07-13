c315b16d133c65acdb42e3f85df384a2a7c376f2
Ido Feins <ifeins@gmail.com>
2012-07-13 22:40:59 +0300

    Implemented ui hash extension for itemview and added specs for it.

--- spec/javascripts/itemView.spec.js
+++ spec/javascripts/itemView.spec.js
@@ -354,4 +354,60 @@ describe("item view", function(){
 
   });
 
+  describe("when an item view has a ui elements hash", function() {
+    describe("accessing a ui element from the hash", function() {
+
+      var View = Backbone.Marionette.ItemView.extend({
+        template: "#item-with-checkbox",
+
+        ui: {
+          checkbox: "#chk",
+          unfoundElement: "#not_found"
+        }
+      });
+
+      var view, chk, model;
+
+      beforeEach(function() {
+        loadFixtures("itemWithCheckbox.html");
+
+        model = new Backbone.Model({
+          done: false
+        });
+
+        debugger;
+        view = new View({
+          model: model
+        });
+
+        view.render();
+      });
+
+
+      it("should return its jQuery selector if it can be found", function(){
+        expect(view.ui.checkbox.attr("type")).toEqual("checkbox");
+      });
+
+      it("should return an empty jQuery object if the it cannot be found", function() {
+        expect(view.ui.unfoundElement.length).toEqual(0);
+      });
+
+      it("should return an up-to-date selector on subsequent renders", function() {
+        // asserting state before subsequent render
+        expect(view.ui.checkbox.attr("checked")).toBeUndefined();
+
+        // setting the model "done" attribute to true will cause the "checked" attribute
+        // to be added to the checkbox element in the subsequent render.
+        view.model.set('done', true);
+        view.render();
+
+        // since the ui elements selectors are refreshed after each render then the associated selector
+        // should point to the newly rendered checkbox element that has the "checked" attribute.
+        expect(view.ui.checkbox.attr("checked")).toBeDefined();
+      });
+
+    });
+
+  });
+
 });

b00c562e951cc9b1b5eade13af4e864eb25ffd84
gartz <gartz@cianet.ind.br>
2012-07-21 18:09:58 -0300

    Fix view triggers to proxy their event to listeners

--- src/backbone.marionette.view.js
+++ src/backbone.marionette.view.js
@@ -78,9 +78,7 @@ Marionette.View = Backbone.View.extend({
     _.each(triggers, function(value, key){
 
       triggerEvents[key] = function(e){
-        if (e && e.preventDefault){ e.preventDefault(); }
-        if (e && e.stopPropagation){ e.stopPropagation(); }
-        that.trigger(value);
+        that.trigger(value, e);
       }
 
     });

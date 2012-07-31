42d9123f91d1ca6dae22894584eca66e0d314885
Deni <liquid.yethee@hotmail.com>
2012-07-31 16:00:16 +0400

    Prevent re-start the module

--- src/backbone.marionette.module.js
+++ src/backbone.marionette.module.js
@@ -43,6 +43,9 @@ _.extend(Marionette.Module.prototype, Backbone.Events, {
 
   // Start the module, and run all of it's initializers
   start: function(options){
+    // Prevent re-start the module
+    if (this._isInitialized){ return; }
+
     this._runModuleDefinition();
     this._initializerCallbacks.run(options, this);
     this._isInitialized = true;

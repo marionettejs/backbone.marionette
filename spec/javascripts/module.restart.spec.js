bdfbab1b8004e141c2dcdfba21827af4233742a7
Derick Bailey <derickbailey@gmail.com>
2012-08-08 08:22:28 -0500

    failing spec to show error with finalizers and restarting modules

--- spec/javascripts/module.restart.spec.js
+++ spec/javascripts/module.restart.spec.js
@@ -29,4 +29,62 @@ describe("module restart", function(){
 
   });
 
+  describe("when defining the module multiple times, starting twice, and stopping once", function(){
+    var MyApp, mod, initializer, finalizer;
+    
+    beforeEach(function(){
+      initializer = jasmine.createSpy();
+      finalizer = jasmine.createSpy();
+
+      MyApp = new Backbone.Marionette.Application();
+      myModule = MyApp.module("MyModule", function(MyMod){
+        MyMod.addInitializer(initializer);
+        MyMod.addFinalizer(finalizer);
+      });
+
+      MyApp.module("MyModule");
+      MyApp.module("MyModule");
+
+      myModule.start();
+      myModule.start();
+      myModule.stop();
+    });
+
+    it("should stop the module once", function(){
+      expect(finalizer.callCount).toBe(1);
+    });
+
+  });
+
+  describe("when a module has a finalizer, starting and stopping the module, then re-starting it", function(){
+    var MyApp, initializer, finalizer;
+    
+    beforeEach(function(){
+      initializer = jasmine.createSpy();
+      finalizer = jasmine.createSpy();
+
+      MyApp = new Backbone.Marionette.Application();
+      MyApp.module("MyModule", function(MyMod){
+        MyMod.addInitializer(initializer);
+        MyMod.addFinalizer(finalizer);
+      });
+
+      var m1 = MyApp.module("MyModule");
+      m1.start();
+      m1.stop();
+
+      var m2 = MyApp.module("MyModule");
+      m2.start();
+    });
+
+    it("should start the module twice", function(){
+      expect(initializer.callCount).toBe(2);
+    });
+
+    it("should stop the module once", function(){
+      expect(finalizer.callCount).toBe(1);
+    });
+
+  });
+
 });

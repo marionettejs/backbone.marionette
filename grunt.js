c2903ed6a17a85bc0e0a35ebbb2b18d4ae3e5724
Jarrod Overson <joverson@onerecovery.com>
2012-09-07 14:52:08 -0700

    tasks for grunt-jasmine-runner

--- grunt.js
+++ grunt.js
@@ -2,6 +2,7 @@
 module.exports = function(grunt) {
 
   grunt.loadNpmTasks('grunt-rigger');
+  grunt.loadNpmTasks('grunt-jasmine-runner');
 
   // Project configuration.
   grunt.initConfig({
@@ -37,6 +38,39 @@ module.exports = function(grunt) {
         src: ['<banner:meta.banner>', '<config:rig.amd.dest>'],
         dest: 'lib/amd/backbone.marionette.min.js'
       }
+    },
+
+    jasmine : {
+      src : [
+        'public/javascripts/jquery.js',
+        'public/javascripts/json2.js',
+        'public/javascripts/underscore.js',
+        'public/javascripts/backbone.js',
+        'src/backbone.marionette.js',
+        'src/async/async.js',
+        'spec/javascripts/support/marionette.support.js',
+        'src/backbone.marionette.eventbinder.js',
+        'src/backbone.marionette.view.js',
+        'src/backbone.marionette.itemview.js',
+        'src/backbone.marionette.collectionview.js',
+        'src/backbone.marionette.compositeview.js',
+        'src/backbone.marionette.region.js',
+        'src/backbone.marionette.layout.js',
+        'src/backbone.marionette.application.js',
+        'src/backbone.marionette.approuter.js',
+        'src/backbone.marionette.module.js',
+        'src/backbone.marionette.templatecache.js',
+        'src/backbone.marionette.renderer.js',
+        'src/backbone.marionette.callbacks.js',
+        'src/backbone.marionette.eventaggregator.js',
+        'src/backbone.marionette.helpers.js'
+      ],
+      helpers : 'spec/javascripts/helpers/*.js',
+      specs : 'spec/javascripts/**/*.spec.js'
+    },
+
+    'jasmine-server' : {
+      browser : false
     },
 
     jshint: {

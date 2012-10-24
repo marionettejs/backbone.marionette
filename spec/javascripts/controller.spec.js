58dac12c1d2b815017f8d8398e726429f2fd3c9c
Derick Bailey <derickbailey@gmail.com>
2012-10-24 12:39:20 -0500

    renamed Object to Controller

--- spec/javascripts/controller.spec.js
+++ spec/javascripts/controller.spec.js
@@ -1,6 +1,40 @@
-describe("controller", function(){
+describe("marionette controller", function(){
 
-  xdescribe("when ... extends from Marionette.Object right now", function(){
+  describe("when creating an controller", function(){
+
+    var Controller = Marionette.Controller.extend({
+      initialize: jasmine.createSpy("initialize method")
+    });
+
+    var controller, options, handler;
+
+    beforeEach(function(){
+      options = {};
+      controller = new Controller(options);
+
+      handler = jasmine.createSpy("foo handler");
+      controller.on("foo", handler);
+
+      controller.trigger("foo", options);
+    });
+
+    it("should support triggering events", function(){
+      expect(handler).toHaveBeenCalledWith(options);
+    });
+
+    it("should have an event aggregator built in to it", function(){
+      expect(controller.eventBinder).not.toBeUndefined();
+      expect(typeof controller.bindTo).toBe("function");
+    });
+
+    it("should support an initialize function", function(){
+      expect(controller.initialize).toHaveBeenCalled();
+    });
+
+    it("should pass constructor options to the initialize function", function(){
+      expect(controller.initialize.mostRecentCall.args[0]).toBe(options);
+    });
+
   });
 
 });
--- src/marionette.controller.js
+++ src/marionette.controller.js
@@ -4,7 +4,18 @@
 // A multi-purpose object to use as a controller for
 // modules and routers, and as a mediator for workflow
 // and coordination of other objects, views, and more.
-Marionette.Controller = Marionette.Object.augment(
-  Marionette.EventBinder,
-  Marionette.EventAggregator
-);
+Marionette.Controller = function(options){
+  if (_.isFunction(this.initialize)){
+    this.initialize(options);
+  }
+
+  Marionette.addEventBinder(this);
+};
+
+Marionette.Controller.extend = Marionette.extend;
+
+// Controller Methods
+// --------------
+
+// Ensure it can trigger events with Backbone.Events
+_.extend(Marionette.Controller.prototype, Backbone.Events);

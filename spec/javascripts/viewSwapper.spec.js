a0c5836fbb0f6818dd6d78b872f1d4ac6bc336af
Derick Bailey <derickbailey@gmail.com>
2012-10-26 09:15:27 -0500

    additional behavior and specs for swapping views, ensuring they are not rendered multiple times

--- spec/javascripts/viewSwapper.spec.js
+++ spec/javascripts/viewSwapper.spec.js
@@ -55,9 +55,141 @@ describe("view swapper", function(){
   });
 
   describe("when a view has already been rendered, and it triggers a configured event", function(){
-    it("should swap to view configured for that event", function(){
-      throw "not yet implemented";
+    var ViewSwapper = Marionette.ViewSwapper.extend({
+      initialView: "firstView",
+      
+      swapOn: {
+        firstView: {
+          "foo:bar": "secondView"
+        }
+      }
     });
+
+    var swapper, v1, v2;
+
+    beforeEach(function(){
+      v1 = new Backbone.Marionette.View();
+      v2 = new Backbone.View();
+
+      spyOn(v1, "close").andCallThrough();
+      spyOn(v2, "render").andCallThrough();
+
+      swapper = new ViewSwapper({
+        views: {
+          firstView: v1,
+          secondView: v2
+        }
+      });
+
+      swapper.render();
+
+      v1.trigger("foo:bar");
+    });
+
+    it("should render the view configured for that event", function(){
+      expect(v2.render).toHaveBeenCalled();
+    });
+
+    it("should populate the swapper's el with the new view", function(){
+      expect(swapper.$el[0].children[0]).toHaveHtml(v1.$el.html());
+    });
+
+    it("should hide the previous view", function(){
+      expect(v1.$el).toBeHidden();
+    });
+
+    it("should not close the previous view", function(){
+      expect(v1.close).not.toHaveBeenCalled();
+    });
+  });
+
+  describe("when swapping to a view that does not exist", function(){
+    var ViewSwapper = Marionette.ViewSwapper.extend({
+      initialView: "firstView",
+      
+      swapOn: {
+        firstView: {
+          "switch:it": "doesn't exist"
+        }
+      }
+    });
+
+    var swapper, v1;
+
+    beforeEach(function(){
+      v1 = new Backbone.Marionette.View();
+
+      swapper = new ViewSwapper({
+        views: {
+          firstView: v1,
+        }
+      });
+
+      swapper.render();
+    });
+
+
+    it("should throw an error saying the view is not found", function(){
+      function run(){ v1.trigger("switch:it"); }
+      expect(run).toThrow("Cannot show view in ViewSwapper. View 'doesn't exist' not found.");
+    });
+
+  });
+
+  describe("when switching to a view and then switching back to the previous view", function(){
+    var ViewSwapper = Marionette.ViewSwapper.extend({
+      initialView: "firstView",
+      
+      swapOn: {
+        firstView: {
+          "foo:bar": "secondView"
+        },
+        secondView: {
+          "switch:back": "firstView"
+        }
+      }
+    });
+
+    var swapper, v1, v2;
+
+    beforeEach(function(){
+      v1 = new Backbone.Marionette.View();
+      v2 = new Backbone.Marionette.View();
+
+      spyOn(v1, "close").andCallThrough();
+      spyOn(v1, "render").andCallThrough();
+      spyOn(v2, "render").andCallThrough();
+      spyOn(v2, "close").andCallThrough();
+
+      swapper = new ViewSwapper({
+        views: {
+          firstView: v1,
+          secondView: v2
+        }
+      });
+
+      swapper.render();
+
+      v1.trigger("foo:bar");
+      v2.trigger("switch:back");
+    });
+
+    it("should not re-render the first view", function(){
+      expect(v1.render.callCount).toBe(1);
+    });
+
+    it("should show the first view, again", function(){
+      expect(swapper.$el[0].children[0]).toHaveHtml(view.$el.html());
+    });
+
+    it("should hide the second view", function(){
+      expect(v2.$el).toBeHidden();
+    });
+
+    it("should not close the second view", function(){
+      expect(v2.close).not.toHaveBeenCalled();
+    });
+
   });
 
 });
--- src/marionette.viewswapper.js
+++ src/marionette.viewswapper.js
@@ -6,9 +6,11 @@
 // place" features, "loading" screens, and more.
 
 Marionette.ViewSwapper = Marionette.View.extend({
-  initialize: function(options){
+  constructor: function(options){
     this.views = options.views;
+    this._swapperViews = {};
     this._setupViewEvents();
+    Marionette.View.prototype.constructor.apply(this, arguments);
   },
 
   // get the view to start with, by looking up the
@@ -21,18 +23,50 @@ Marionette.ViewSwapper = Marionette.View.extend({
     return this._getView(viewName);
   },
 
+  // Get a view by name, throwing an exception if the view instance
+  // is not found.
   _getView: function(viewName){
-    var view = this.views[viewName];
+    var originalView;
+    var swapperView = this._swapperViews[viewName];
 
-    if (!view){
-      var error = Error("Cannot show view in ViewSwapper. View '" + viewName + "' not found.");
-      error.name = "ViewNotFoundError";
-      throw error;
+    if (!swapperView){
+      originalView = this.views[viewName];
+
+      if (!originalView){
+        var error = Error("Cannot show view in ViewSwapper. View '" + viewName + "' not found.");
+        error.name = "ViewNotFoundError";
+        throw error;
+      }
+
+      swapperView = this._buildSwapperView(originalView);
+      this._swapperViews[viewName] = swapperView;
     }
 
-    return view;
+    return swapperView;
   },
 
+  // Decorate the configured view with information that the view swapper
+  // needs, to keep track of the view's current state.
+  _buildSwapperView: function(originalView){
+    var swapperView = Object.create(originalView);
+    _.extend(swapperView, {
+      
+      // Prevent the underlying view from being rendered more than once
+      render: function(){
+        if (this._hasBeenRendered){
+          return this;
+        } else {
+          this._hasBeenRendered = true;
+          return originalView.render.apply(originalView, arguments);
+        }
+      }
+
+    });
+    return swapperView;
+  },
+
+  // Set up the event handlers for the individual views, so that the
+  // swapping can happen when a view event is triggered
   _setupViewEvents: function(){
     var that = this;
     _.each(this.swapOn, function(config, viewName){
@@ -44,17 +78,20 @@ Marionette.ViewSwapper = Marionette.View.extend({
     });
   },
 
+  // Do the swapping of the views to the new view, by name
   _swapView: function(viewName){
     if (this.currentView){
       this.currentView.$el.hide();
     }
 
-    var view = this.views[viewName];
+    var view = this._getView(viewName);
 
     this.currentView = view;
     this.render();
   },
 
+  // Render the current view. If no current view is set, it
+  // will render the `initialView` that was configured.
   render: function(){
     if (!this.currentView){
       this.currentView = this._getInitialView();

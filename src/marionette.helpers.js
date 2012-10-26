7ffeacb23e7b966aba23d8bf77301f480513b1aa
Derick Bailey <derickbailey@gmail.com>
2012-10-26 13:34:02 -0500

    optimizations for the Object.create shim

--- src/marionette.helpers.js
+++ src/marionette.helpers.js
@@ -11,19 +11,41 @@ Marionette.extend = Backbone.Model.extend;
 // if available, otherwise shims it in place for Marionette to use.
 Marionette.createObject = (function(){
   var createObject;
+  
+  // Define this once, and just replace the .prototype on it as needed,
+  // to improve performance in older / less optimized JS engines
+  function F() {}
 
+
+  // Check for existing native / shimmed Object.create
   if (typeof Object.create === "function"){
 
-    // found native, use it
+    // found native/shim, so use it
     createObject = Object.create;
 
   } else {
 
-    // native not found, shim it
+    // An implementation of the Boodman/Crockford delegation 
+    // w/ Cornford optimization, as suggested by @unscriptable
+    // https://gist.github.com/3959151
+
+    // native/shim not found, so shim it ourself
     createObject = function (o) {
-      function F() {}
+
+      // set the prototype of the function
+      // so we will get `o` as the prototype
+      // of the new object instance
       F.prototype = o;
-      return new F();
+
+      // create a new object that inherits from
+      // the `o` parameter
+      var child = new F();
+      
+      // clean up just in case o is really large
+      F.prototype = null; 
+
+      // send it back
+      return child;
     };
 
   }

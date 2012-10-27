03f7d69bdeaf0dd393e858a070695de50ac45233
Derick Bailey <derickbailey@gmail.com>
2012-10-27 14:25:13 -0500

    added Marionette.getOption method

--- src/marionette.helpers.js
+++ src/marionette.helpers.js
@@ -6,3 +6,21 @@ var slice = Array.prototype.slice;
 
 // Borrow the Backbone `extend` method so we can use it as needed
 Marionette.extend = Backbone.Model.extend;
+
+// Marionette.getOption
+// --------------------
+
+// Retrieve an object, function or other value from a target
+// object or it's `options`, with `options` taking precedence.
+Marionette.getOption = function(target, optionName){
+  if (!target || !optionName){ return; }
+  var value;
+
+  if (target.options && target.options[optionName]){
+    value = target.options[optionName];
+  } else {
+    value = target[optionName];
+  }
+
+  return value;
+}

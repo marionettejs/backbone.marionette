268b56fe6b27b30ea1796233d2f7262b28e9d7d0
Derick Bailey <derickbailey@gmail.com>
2013-02-02 13:16:34 -0600

    working on spec for beforeRender in composite views... can't get it to fail for the right reasons, yet

--- spec/javascripts/compositeView.onBeforeRender.spec.js
+++ spec/javascripts/compositeView.onBeforeRender.spec.js
@@ -1,77 +1,57 @@
 describe("composite view - on before render", function(){
 
   describe("when a composite view has a model and a collection", function(){
-    var order;
+    var view;
+
+    var Label = {
+        Views: {}
+    };
+
+    Label.Model = Backbone.Model.extend({});
+    Label.ListModel = Backbone.Model.extend({});
+
+    Label.Collection = Backbone.Collection.extend({
+        model: Label.Model
+    });
+
+    Label.Views.LabelItem = Backbone.Marionette.ItemView.extend({
+        template: function(){
+          return _.template('Title: <%= title %> - Bar : <%= bar %>');
+        },
+        tagName: 'li',
+        className: 'list-item'
+    });
+
+    Label.Views.LabelListModel = Backbone.Model.extend({});
+
+    Label.Views.LabelList = Backbone.Marionette.CompositeView.extend({
+      template: function(){
+        return _.template('<div class="listing"> <h4><%= modelState %></h4> <br/> <ul id="listTag"> </ul> </div>');
+      },
+
+      itemViewContainer: '#listTag',
+
+      itemView: Label.Views.LabelItem,
+
+      onBeforeRender : function() {
+        this.model.set("modelState", "Something Different");
+      }
+    });
 
     beforeEach(function(){
-      var Label = {
-          Views: {}
-      };
+      var collection = new Label.Collection([{ title: 'yoddle' }, { title: 'little' }]);
+      var model = new Label.Model({ modelState: 'Yoddling Tomes' });
 
-      Label.Model = Backbone.Model.extend({});
-      Label.ListModel = Backbone.Model.extend({});
-
-      Label.Collection = Backbone.Collection.extend({
-          model: Label.Model
+      view = new Label.Views.LabelList({
+        collection: collection,
+        model: model
       });
 
-      Label.Views.LabelItem = Backbone.Marionette.ItemView.extend({
-          template: function(){
-            return 'Title: <%= title %> - Bar : <%= bar %>'
-          },
-          tagName: 'li',
-          className: 'list-item',
-          onBeforeRender : function() {
-            console.log("item view: before render");
-          },
-
-          onRender: function(){
-            console.log("item view: after render");
-          }
-          
-      });
-
-      Label.Views.LabelListModel = Backbone.Model.extend({});
-
-      Label.Views.LabelList = Backbone.Marionette.CompositeView.extend({
-        template: function(){
-          return '<div class="listing"> <h4><%=modelState%></h4> <br/> <ul id="listTag"> </ul> </div>';
-        },
-        itemViewContainer: '#listTag',
-        itemView: Label.Views.LabelItem,
-
-        serializeData: function(){
-          console.log("composite view: serialize data");
-          return Marionette.CompositeView.prototype.serializeData.call(this);
-        },
-        onBeforeRender : function() {
-          console.log("composite view: before render");
-        },
- 
-        onRender: function(){
-          console.log("composite view: after render");
-        }
-      });
-
-      var collection = new Label.Collection([{
-          title: 'yoddle'
-      }, {
-          title: 'little'
-      }]);
-      var model = new Label.Model({
-          modelState: 'Yoddling Tomes'
-      });
-      var view = new Label.Views.LabelList({
-          collection: collection,
-          model: model
-      });
-
-      var region = new Marionette.Region({el: "<div></div>"});
-      region.show(view);
+      view.render();
     });
 
     it("should call onBeforeRender before rendering the model", function(){
-      throw new "need to write a real test here";
+      expect(view.$el).toHaveText(/Yoddling Tomes/);
     });
 
   });

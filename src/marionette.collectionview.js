7c9e76e2a39c97968c9c0be9fe735d5f3856ebbd
Paul Young <paulyoungonline@gmail.com>
2013-02-19 12:19:50 -0500

    Fixes #480: Passing index to ItemViewOptions when it is a function.

--- src/marionette.collectionview.js
+++ src/marionette.collectionview.js
@@ -141,7 +141,7 @@ Marionette.CollectionView = Marionette.View.extend({
     // get the itemViewOptions if any were specified
     var itemViewOptions = Marionette.getOption(this, "itemViewOptions");
     if (_.isFunction(itemViewOptions)){
-      itemViewOptions = itemViewOptions.call(this, item);
+      itemViewOptions = itemViewOptions.call(this, item, index);
     }
 
     // build the view 

5ccd91a0f7177fdabf31f83ca8f0f45d3e64aae9
Jarrod Overson <joverson@onehealth.com>
2013-02-22 16:14:41 -0800

    added dev task, watch, and separated coverage and basic marionette test

--- Gruntfile.js
+++ Gruntfile.js
@@ -76,46 +76,51 @@ module.exports = function(grunt) {
     },
 
     jasmine : {
-      marionette : {
-      src : [
-        'src/build/marionette.core.js',
-        'spec/javascripts/support/marionette.support.js',
-        'src/marionette.helpers.js',
-        'src/marionette.createObject.js',
-        'src/marionette.triggermethod.js',
-        'src/marionette.bindEntityEvents.js',
-        'src/marionette.controller.js',
-        'src/marionette.domRefresh.js',
-        'src/marionette.view.js',
-        'src/marionette.itemview.js',
-        'src/marionette.collectionview.js',
-        'src/marionette.compositeview.js',
-        'src/marionette.region.js',
-        'src/marionette.layout.js',
-        'src/marionette.application.js',
-        'src/marionette.approuter.js',
-        'src/marionette.module.js',
-        'src/marionette.templatecache.js',
-        'src/marionette.renderer.js',
-        'src/marionette.callbacks.js'
-      ],
+      options : {
+        helpers : 'spec/javascripts/helpers/*.js',
+        specs : 'spec/javascripts/**/*.spec.js',
+        vendor : [
+          'public/javascripts/jquery.js',
+          'public/javascripts/json2.js',
+          'public/javascripts/underscore.js',
+          'public/javascripts/backbone.js',
+          'public/javascripts/backbone.babysitter.js',
+          'public/javascripts/backbone.wreqr.js',
+        ],
+      },
+      coverage : {
+        src : '<%= jasmine.marionette.src %>',
         options : {
           template : require('grunt-template-jasmine-istanbul'),
-          helpers : 'spec/javascripts/helpers/*.js',
-          specs : 'spec/javascripts/**/*.spec.js',
-          vendor : [
-            'public/javascripts/jquery.js',
-            'public/javascripts/json2.js',
-            'public/javascripts/underscore.js',
-            'public/javascripts/backbone.js',
-            'public/javascripts/backbone.babysitter.js',
-            'public/javascripts/backbone.wreqr.js',
-          ],
           templateOptions: {
             coverage: 'reports/coverage.json',
             report: 'reports/coverage'
           }
         }
+      },
+      marionette : {
+        src : [
+          'src/build/marionette.core.js',
+          'spec/javascripts/support/marionette.support.js',
+          'src/marionette.helpers.js',
+          'src/marionette.createObject.js',
+          'src/marionette.triggermethod.js',
+          'src/marionette.bindEntityEvents.js',
+          'src/marionette.controller.js',
+          'src/marionette.domRefresh.js',
+          'src/marionette.view.js',
+          'src/marionette.itemview.js',
+          'src/marionette.collectionview.js',
+          'src/marionette.compositeview.js',
+          'src/marionette.region.js',
+          'src/marionette.layout.js',
+          'src/marionette.application.js',
+          'src/marionette.approuter.js',
+          'src/marionette.module.js',
+          'src/marionette.templatecache.js',
+          'src/marionette.renderer.js',
+          'src/marionette.callbacks.js'
+        ],
       }
     },
 
@@ -134,6 +139,12 @@ module.exports = function(grunt) {
         }
       }
     },
+    watch: {
+      marionette : {
+        files : ['src/*.js', 'spec/**/*.js'],
+        tasks : ['jshint', 'jasmine:marionette']
+      }
+    }
   });
 
   grunt.loadNpmTasks('grunt-preprocess');
@@ -141,11 +152,14 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-plato');
 
-  grunt.registerTask('test', ['jshint', 'jasmine']);
+  grunt.registerTask('test', ['jshint', 'jasmine:marionette']);
+
+  grunt.registerTask('dev', ['test', 'watch']);
 
   // Default task.
-  grunt.registerTask('default', ['jshint', 'preprocess', 'concat', 'uglify']);
+  grunt.registerTask('default', ['jshint', 'jasmine:coverage', 'preprocess', 'concat', 'uglify']);
 
 };

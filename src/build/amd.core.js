4f42ef1e0d9052715f3c8a296d16e77740c0bcce
Derick Bailey <derickbailey@gmail.com>
2013-03-24 14:32:58 -0500

    removed jQuery from AMD requirments

--- src/build/amd.core.js
+++ src/build/amd.core.js
@@ -1,20 +1,19 @@
 (function (root, factory) {
   if (typeof exports === 'object') {
 
-    var jquery = require('jquery');
     var underscore = require('underscore');
     var backbone = require('backbone');
     var wreqr = require('backbone.wreqr');
     var babysitter = require('backbone.babysitter');
 
-    module.exports = factory(jquery, underscore, backbone, wreqr, babysitter);
+    module.exports = factory(underscore, backbone, wreqr, babysitter);
 
   } else if (typeof define === 'function' && define.amd) {
 
-    define(['jquery', 'underscore', 'backbone', 'backbone.wreqr', 'backbone.babysitter'], factory);
+    define(['underscore', 'backbone', 'backbone.wreqr', 'backbone.babysitter'], factory);
 
   } 
-}(this, function ($, _, Backbone) {
+}(this, function (_, Backbone) {
 
   // @include marionette.core.js
   return Backbone.Marionette; 

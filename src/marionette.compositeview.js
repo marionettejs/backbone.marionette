9cc6d6f566d8c707c56c7273fa8a833fdbd86fdd
Martin Hewitt <martin.hewitt@surevine.com>
2013-07-11 10:37:44 +0100

    Updated Renderer, CompositeView and ItemView such that when template() is called, it's in the context of the relevant view, not Window.

--- src/marionette.compositeview.js
+++ src/marionette.compositeview.js
@@ -90,7 +90,7 @@ Marionette.CompositeView = Marionette.CollectionView.extend({
     data = this.mixinTemplateHelpers(data);
 
     var template = this.getTemplate();
-    return Marionette.Renderer.render(template, data);
+    return Marionette.Renderer.render(template, data, this);
   },
 
   // Appends the `el` of itemView instances to the specified

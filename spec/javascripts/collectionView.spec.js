ea298f57ee2fe6e99c9210941d78120c6a01c867
Paul Tyng <paul@paultyng.net>
2013-10-10 17:14:25 -0400

    Remove extra whitespace

--- spec/javascripts/collectionView.spec.js
+++ spec/javascripts/collectionView.spec.js
@@ -3,7 +3,7 @@ describe("collection view", function(){
 
   // Shared View Definitions
   // -----------------------
-  
+
   var ItemView = Backbone.Marionette.ItemView.extend({
     tagName: "span",
     render: function(){
@@ -26,7 +26,7 @@ describe("collection view", function(){
 
   // Collection View Specs
   // ---------------------
-  
+
   describe("when rendering a collection view with no `itemView` specified", function(){
     var NoItemView = Backbone.Marionette.CollectionView.extend({
     });
@@ -44,7 +44,7 @@ describe("collection view", function(){
       expect(function(){collectionView.render()}).toThrow("An `itemView` must be specified");
     });
   });
-  
+
   describe("when rendering a collection view", function(){
     var collection = new Backbone.Collection([{foo: "bar"}, {foo: "baz"}]);
     var collectionView, itemViewRender;
@@ -91,7 +91,7 @@ describe("collection view", function(){
     it("should trigger a 'before:render' event", function(){
       expect(collectionView.trigger).toHaveBeenCalledWith("before:render", collectionView);
     });
-    
+
     it("should trigger a 'collection:before:render' event", function(){
       expect(collectionView.trigger).toHaveBeenCalledWith("collection:before:render", collectionView);
     });
@@ -99,7 +99,7 @@ describe("collection view", function(){
     it("should trigger a 'collection:rendered' event", function(){
       expect(collectionView.trigger).toHaveBeenCalledWith("collection:rendered", collectionView);
     });
-    
+
     it("should trigger a 'render' event", function(){
       expect(collectionView.trigger).toHaveBeenCalledWith("render", collectionView);
     });
@@ -311,7 +311,7 @@ describe("collection view", function(){
 
       onClose: function(){ }
     });
-  
+
     var collectionView;
     var collection;
     var childView;
@@ -345,7 +345,7 @@ describe("collection view", function(){
       spyOn(collectionView, "onClose").andCallThrough();
       spyOn(collectionView, "onBeforeClose").andCallThrough();
       spyOn(collectionView, "trigger").andCallThrough();
-      
+
       collectionView.bind('collection:closed', closeHandler);
 
       collectionView.close();
@@ -706,21 +706,21 @@ describe("collection view", function(){
 
       iv = cv.children.findByModel(c.at(0));
     });
-    
+
     it("should use the specified itemView for each item", function(){
       expect(iv.MyItemView).toBe(true);
     });
   });
-  
+
   describe("has a valid inheritance chain back to Marionette.View", function(){
-    
+
     var constructor;
-    
+
     beforeEach(function(){
       constructor = spyOn(Marionette.View.prototype, "constructor");
       new Marionette.CollectionView();
     });
-    
+
     it("calls the parent Marionette.View's constructor function on instantiation", function(){
       expect(constructor).toHaveBeenCalled();
     });

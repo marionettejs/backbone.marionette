2e4069675358e718bd0791199e15438fe8cce7ed
Sam Saccone <samccone@gmail.com>
2014-04-03 20:55:37 -0400

    update behavior inline documentation

--- src/marionette.behavior.js
+++ src/marionette.behavior.js
@@ -1,10 +1,24 @@
+// Behavior
+// -----------
+
+// A Behavior is an isolated set of DOM /
+// user interactions that can be mixed into any View.
+// Behaviors allow you to blackbox View specific interactions
+// into portable logical chunks, keeping your views simple and your code DRY.
+
 Marionette.Behavior = (function(_, Backbone){
   function Behavior(options, view){
+    // Setup reference to the view.
+    // this comes in handy when a behavior
+    // wants to directly talk up the chain
+    // to the view.
     this.view = view;
     this.defaults = _.result(this, "defaults") || {};
     this.options  = _.extend({}, this.defaults, options);
 
     // proxy behavior $ method to the view
+    // this is useful for doing jquery DOM lookups
+    // scoped to behaviors view.
     this.$ = function() {
       return this.view.$.apply(this.view, arguments);
     };
@@ -15,6 +29,7 @@ Marionette.Behavior = (function(_, Backbone){
   _.extend(Behavior.prototype, {
     initialize: function(){},
 
+    // Setup class level extend for triggerMethod.
     triggerMethod: Marionette.triggerMethod
   });
 

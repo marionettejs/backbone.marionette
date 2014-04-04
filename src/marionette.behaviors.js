023888cd869cd36b7bd0a9abd7a91f47345ff3e5
Sam Saccone <samccone@gmail.com>
2014-04-03 21:10:40 -0400

    improve inline documentation for behaviors

--- src/marionette.behaviors.js
+++ src/marionette.behaviors.js
@@ -1,3 +1,12 @@
+// Marionette.Behaviors
+// --------
+
+// Behaviors is a utility class that takes care of
+// glueing your behavior instances to their given View.
+// The most important part of this class is that you
+// **MUST** override the class level behaviorsLookup
+// method for things to work properly.
+
 Marionette.Behaviors = (function(Marionette, _) {
 
   function Behaviors(view) {
@@ -5,6 +14,9 @@ Marionette.Behaviors = (function(Marionette, _) {
     // or it can be a function that returns an object.
     this.behaviors = Behaviors.parseBehaviors(view, _.result(view, 'behaviors'));
 
+    // Wraps several of the view's methods
+    // calling the methods first on each behavior
+    // and then eventually calling the method on the view.
     Behaviors.wrap(view, this.behaviors, [
       'bindUIElements', 'unbindUIElements',
       'delegateEvents', 'undelegateEvents',
@@ -18,7 +30,9 @@ Marionette.Behaviors = (function(Marionette, _) {
     setElement: function(setElement, behaviors) {
       setElement.apply(this, _.tail(arguments, 2));
 
-      // proxy behavior $el to the view's $el
+      // proxy behavior $el to the view's $el.
+      // This is needed because a view's $el proxy
+      // is not set until after setElement is called.
       _.each(behaviors, function(b) {
         b.$el = this.$el;
       }, this);
@@ -95,14 +109,24 @@ Marionette.Behaviors = (function(Marionette, _) {
         var _events = {};
         var behaviorEvents = _.result(b, 'events') || {};
         var behaviorUI = _.result(b, 'ui');
+
+        // Construct an internal UI hash first using
+        // the views UI hash and then the behaviors UI hash.
+        // This allows the user to use UI hash elements
+        // defined in the parent view as well as those
+        // defined in the given behavior.
         var ui = _.extend({}, viewUI, behaviorUI);
 
+        // Normalize behavior events hash to allow
+        // a user to use the @ui. syntax.
         behaviorEvents = Marionette.normalizeUIKeys(behaviorEvents, ui);
 
         _.each(_.keys(behaviorEvents), function(key) {
           // append white-space at the end of each key to prevent behavior key collisions
           // this is relying on the fact backbone events considers "click .foo" the same  "click .foo "
           // starts with an array of two so the first behavior has one space
+
+          // +2 is uses becauce new Array(1) or 0 is "" and not " "
           var whitespace = (new Array(i+2)).join(" ");
           var eventKey   = key + whitespace;
           var handler    = _.isFunction(behaviorEvents[key]) ? behaviorEvents[key] : b[behaviorEvents[key]];
@@ -130,6 +154,11 @@ Marionette.Behaviors = (function(Marionette, _) {
       throw new Error("You must define where your behaviors are stored. See https://github.com/marionettejs/backbone.marionette/blob/master/docs/marionette.behaviors.md#behaviorslookup");
     },
 
+    // Takes care of getting the behavior class
+    // given options and a key.
+    // If a user passes in options.behaviorClass
+    // default to using that. Otherwise delegate
+    // the lookup to the users behaviorsLookup implementation.
     getBehaviorClass: function(options, key) {
       if (options.behaviorClass) {
         return options.behaviorClass;
@@ -139,6 +168,9 @@ Marionette.Behaviors = (function(Marionette, _) {
       return _.isFunction(Behaviors.behaviorsLookup) ? Behaviors.behaviorsLookup.apply(this, arguments)[key] : Behaviors.behaviorsLookup[key];
     },
 
+    // Maps over a view's behaviors. Performing
+    // a lookup on each behavior and the instantiating
+    // said behavior passing its options and view.
     parseBehaviors: function(view, behaviors){
       return _.map(behaviors, function(options, key){
         var BehaviorClass = Behaviors.getBehaviorClass(options, key);

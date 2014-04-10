b7cc9b3fde2bb0d22a08da419793d30d581d9be7
Andrew Humphreys <andrew.humphreys@bedegaming.com>
2014-04-10 20:50:11 +0100

    s/preventDestroy/preventClose

--- src/marionette.region.js
+++ src/marionette.region.js
@@ -116,17 +116,17 @@ _.extend(Marionette.Region.prototype, Backbone.Events, {
   // directly from the `el` attribute. Also calls an optional
   // `onShow` and `close` method on your view, just after showing
   // or just before closing the view, respectively.
-  // The `preventDestroy` option can be used to prevent a view from being destroyed on show.
+  // The `preventClose` option can be used to prevent a view from being destroyed on show.
   show: function(view, options){
     this.ensureEl();
 
     var showOptions = options || {};
     var isViewClosed = view.isClosed || _.isUndefined(view.$el);
     var isDifferentView = view !== this.currentView;
-    var preventDestroy =  !!showOptions.preventDestroy;
+    var preventClose =  !!showOptions.preventClose;
 
-    // only close the view if we don't want to preventDestroy and the view is different
-    var _shouldCloseView = !preventDestroy && isDifferentView;
+    // only close the view if we don't want to preventClose and the view is different
+    var _shouldCloseView = !preventClose && isDifferentView;
 
     if (_shouldCloseView) {
       this.close();

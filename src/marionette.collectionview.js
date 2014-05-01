e06f4191b91207a71b191346743874ea6f331e64
Jason Laster <jason.laster.11@gmail.com>
2014-05-01 11:09:04 -0400

    Update Collection onChildAdd to _onCollectionAdd

--- src/marionette.collectionview.js
+++ src/marionette.collectionview.js
@@ -56,7 +56,7 @@ Marionette.CollectionView = Marionette.View.extend({
   // binds to.
   _initialEvents: function() {
     if (this.collection) {
-      this.listenTo(this.collection, 'add', this.onChildAdd);
+      this.listenTo(this.collection, 'add', this._onCollectionAdd);
       this.listenTo(this.collection, 'remove', this.onChildRemove);
       this.listenTo(this.collection, 'reset', this.render);
 
@@ -67,7 +67,7 @@ Marionette.CollectionView = Marionette.View.extend({
   },
 
   // Handle a child added to the collection
-  onChildAdd: function(child, collection, options) {
+  _onCollectionAdd: function(child, collection, options) {
     this.destroyEmptyView();
     var ChildView = this.getChildView(child);
     var index = this.collection.indexOf(child);

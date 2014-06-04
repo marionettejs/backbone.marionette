bad474898f2d578df77fafc2863e2f855d966283
James Kyle <me@thejameskyle.com>
2014-06-04 17:21:30 -0400

    Cleaned up requestResponse spec

--- spec/javascripts/requestResponse.spec.js
+++ spec/javascripts/requestResponse.spec.js
@@ -1,17 +1,17 @@
 describe('application request/response', function() {
   'use strict';
 
-  describe('when creating an instance of an Application', function() {
-    beforeEach(function() {
-      this.App = new Marionette.Application();
-    });
+  beforeEach(function() {
+    this.app = new Marionette.Application();
+  });
 
+  describe('when creating an instance of an Application', function() {
     it('should provide request/response framework', function() {
-      expect(this.App.reqres).to.be.instanceof(Backbone.Wreqr.RequestResponse);
+      expect(this.app.reqres).to.be.an.instanceof(Backbone.Wreqr.RequestResponse);
     });
 
     it('should allow direct request', function() {
-      expect(typeof this.App.request).to.equal('function');
+      expect(this.app.request).to.be.a('function');
     });
   });
 });

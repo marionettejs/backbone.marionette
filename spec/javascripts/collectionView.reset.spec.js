eb3a405a2f98efb73ed21f5940e47fced3dc60c8
James Kyle <me@thejameskyle.com>
2014-06-03 22:48:57 -0400

    Cleaned up collectionView.reset spec

--- spec/javascripts/collectionView.reset.spec.js
+++ spec/javascripts/collectionView.reset.spec.js
@@ -2,63 +2,50 @@ describe('collection view - reset', function() {
   'use strict';
 
   beforeEach(function() {
+    this.collection = new Backbone.Collection();
+
     this.ItemView = Backbone.Marionette.ItemView.extend({
-      tagName: 'span',
-      render: function() {
-        this.$el.html(this.model.get('foo'));
-        this.trigger('render');
-      },
-      onRender: function() {}
+      template: _.template('<%= foo %>')
     });
+
+    this.onRenderStub = this.sinon.stub();
+    this.destroyChildrenStub = this.sinon.stub();
 
     this.CollectionView = Backbone.Marionette.CollectionView.extend({
       childView: this.ItemView,
-      onBeforeRender: function() {},
-      onRender: function() {},
-      onBeforeChildAdded: function() {},
-      onAfterChildAdded: function() {}
+      onRender: this.onRenderStub,
+      destroyChildren: this.destroyChildrenStub
+    });
+
+    this.collectionView = new this.CollectionView({
+      collection: this.collection
     });
   });
 
   describe('when a collection is reset after the view is loaded', function() {
     beforeEach(function() {
-      this.collection = new Backbone.Collection();
-
-      this.collectionView = new this.CollectionView({
-        collection: this.collection
-      });
-
-      this.sinon.spy(this.collectionView, 'onRender');
-      this.sinon.spy(this.collectionView, 'destroyChildren');
-
       this.collectionView.render();
-
       this.collection.reset([{foo: 'bar'}, {foo: 'baz'}]);
     });
 
     it('should destroy all open child views', function() {
-      expect(this.collectionView.destroyChildren).to.have.been.called;
+      expect(this.destroyChildrenStub).to.have.been.called;
     });
 
     it('should append the html for each childView', function() {
-      expect($(this.collectionView.$el)).to.have.$html('<span>bar</span><span>baz</span>');
+      expect(this.collectionView.$el).to.have.$html('<div>bar</div><div>baz</div>');
     });
 
     it('should reference each of the rendered view items', function() {
-      expect(this.collectionView.children.length).to.equal(2);
+      expect(this.collectionView.children).to.have.lengthOf(2);
     });
 
     it('should call "onRender" after rendering', function() {
-      expect(this.collectionView.onRender).to.have.been.called;
+      expect(this.onRenderStub).to.have.been.called;
     });
 
     it('should remove the event handlers for the original children', function() {
-      // maintain backwards compatibility with backbone 1.0.0 in tests
-      if (typeof this.collectionView._listeningTo !== 'undefined') {
-        expect(_.size(this.collectionView._listeningTo)).to.equal(4);
-      } else {
-        expect(_.size(this.collectionView._listeners)).to.equal(4);
-      }
+      expect(_.toArray(this.collectionView._listeningTo)).to.have.lengthOf(4);
     });
   });
 });

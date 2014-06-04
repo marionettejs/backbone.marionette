bb0df2b201e763165615f38bf97153ced8b51dd6
James Kyle <me@thejameskyle.com>
2014-06-04 09:30:35 -0400

    Cleaned up getOption spec

--- spec/javascripts/getOption.spec.js
+++ spec/javascripts/getOption.spec.js
@@ -3,96 +3,68 @@ describe('get option', function() {
 
   describe('when an object only has the option set on the definition', function() {
     beforeEach(function() {
-      this.target = {
-        foo: 'bar'
-      };
-
+      this.target = { foo: 'bar' };
       this.value = Marionette.getOption(this.target, 'foo');
     });
 
     it('should return that definitions option', function() {
-      expect(this.value).to.equal('bar');
+      expect(this.value).to.equal(this.target.foo);
     });
   });
 
   describe('when an object only has the option set on the options', function() {
     beforeEach(function() {
-      this.target = {
-        options: {
-          foo: 'bar'
-        }
-      };
-
+      this.target = { options: { foo: 'bar' } };
       this.value = Marionette.getOption(this.target, 'foo');
     });
 
     it('should return value from the options', function() {
-      expect(this.value).to.equal('bar');
+      expect(this.value).to.equal(this.target.options.foo);
     });
   });
 
   describe('when an object has the option set on the options, and it is a "falsey" value', function() {
     beforeEach(function() {
-      this.target = {
-        options: {
-          foo: false
-        }
-      };
-
+      this.target = { options: { foo: false } };
       this.value = Marionette.getOption(this.target, 'foo');
     });
 
     it('should return value from the options', function() {
-      expect(this.value).to.equal(false);
+      expect(this.value).to.equal(this.target.options.foo);
     });
   });
 
   describe('when an object has the option set on the options, and it is a "undefined" value', function() {
     beforeEach(function() {
-      this.target = {
-        options: {
-          foo: undefined
-        },
-
-        foo: 'bar'
-      };
-
+      this.target = { foo: 'bar', options: { foo: undefined } };
       this.value = Marionette.getOption(this.target, 'foo');
     });
 
     it('should return the objects value', function() {
-      expect(this.value).to.equal('bar');
+      expect(this.value).to.equal(this.target.foo);
     });
   });
 
   describe('when an object has the option set on both the defininition and options', function() {
     beforeEach(function() {
-      this.target = {
-        foo: 'bar',
-
-        options: {
-          foo: 'quux'
-        }
-      };
-
+      this.target = { foo: 'bar', options: { foo: 'baz' } };
       this.value = Marionette.getOption(this.target, 'foo');
     });
 
     it('should return that value from the options', function() {
-      expect(this.value).to.equal('quux');
+      expect(this.value).to.equal(this.target.options.foo);
     });
   });
 
   describe('when proxying getOption', function() {
     beforeEach(function() {
-      this.target = {
-        foo: 'bar',
-        getOption: Marionette.proxyGetOption
-      };
+      this.target = { foo: 'bar' };
+      this.target.getOption = Marionette.proxyGetOption;
+      this.value = this.target.getOption('foo');
     });
 
-    it('should return that definition\'s option', function(){
-      expect(this.target.getOption('foo')).to.equal('bar');
+    it('should return that definition\'s option', function() {
+      expect(this.value).to.equal(this.target.foo);
     });
   });
 });

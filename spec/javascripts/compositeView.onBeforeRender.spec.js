33c3f37559c421b4953b241442053e9629636fc6
James Kyle <me@thejameskyle.com>
2014-06-03 23:16:17 -0400

    Cleaned up compositeView.onBeforeRender spec

--- spec/javascripts/compositeView.onBeforeRender.spec.js
+++ spec/javascripts/compositeView.onBeforeRender.spec.js
@@ -1,53 +1,20 @@
 describe('composite view - on before render', function() {
   'use strict';
 
-  describe('when a composite view has a model and a collection', function() {
-    beforeEach(function() {
-      this.Label = {
-        Views: {}
-      };
+  beforeEach(function() {
+    this.onBeforeRenderStub = this.sinon.stub();
+    this.renderRootStub = this.sinon.stub();
 
-      this.Label.Model = Backbone.Model.extend({});
-      this.Label.ListModel = Backbone.Model.extend({});
-
-      this.Label.Collection = Backbone.Collection.extend({
-        model: this.Label.Model
-      });
-
-      this.Label.Views.LabelItem = Backbone.Marionette.ItemView.extend({
-        template: '#itemView',
-        tagName: 'li',
-        className: 'list-item'
-      });
-
-      this.Label.Views.LabelListModel = Backbone.Model.extend({});
-
-      this.Label.Views.LabelList = Backbone.Marionette.CompositeView.extend({
-        template: '#compView',
-        childViewContainer: '#listTag',
-
-        childView: this.Label.Views.LabelItem,
-
-        onBeforeRender: function() {
-          this.model.set('modelState', 'Something Different');
-        }
-      });
-
-      this.setFixtures('<script id="itemView" type="text/template">Title: <%= title %> </script><script id="compView" type="text/template"><div class="listing"> <h4><%= modelState %></h4> <br/> <ul id="listTag"> </ul> </div></script>');
-
-      this.collection = new this.Label.Collection([{title: 'yoddle'}, {title: 'little'}]);
-      this.model = new this.Label.Model({modelState: 'Yoddling Tomes'});
-
-      this.view = new this.Label.Views.LabelList({
-        collection: this.collection,
-        model: this.model
-      });
-
-      this.view.render();
+    this.CompositeView = Marionette.CompositeView.extend({
+      onBeforeRender: this.onBeforeRenderStub,
+      _renderRoot: this.renderRootStub
     });
 
-    it('should call onBeforeRender before rendering the model', function() {
-      expect(this.view.$el).to.contain.$text('Something Different');
-    });
+    this.compositeView = new this.CompositeView();
+    this.compositeView.render();
+  });
+
+  it('should call onBeforeRender before rendering the model', function() {
+    expect(this.renderRootStub).to.have.been.calledAfter(this.onBeforeRenderStub);
   });
 });

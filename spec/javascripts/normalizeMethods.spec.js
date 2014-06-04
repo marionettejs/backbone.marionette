19dc65fb514f12e974bd2aadfd640e0983ef71d7
James Kyle <me@thejameskyle.com>
2014-06-04 10:03:43 -0400

    Cleaned up normalizeMethods spec

--- spec/javascripts/normalizeMethods.spec.js
+++ spec/javascripts/normalizeMethods.spec.js
@@ -1,35 +1,25 @@
 describe('normalizeMethods', function() {
   'use strict';
 
+  beforeEach(function() {
+    this.View = Backbone.Marionette.ItemView.extend({
+      foo: this.sinon.stub()
+    });
+    this.view = new this.View();
+    this.hash = {
+      'foo': 'foo',
+      'bar': 'bar'
+    };
+    this.normalizedHash = this.view.normalizeMethods(this.hash);
+  });
+
   describe('when normalizeMethods is called with a hash of functions and strings', function() {
-    beforeEach(function() {
-      this.View = Backbone.Marionette.ItemView.extend({
-        initialize: function(options) {
-          this.two = function() {};
-          var hash = _.extend({
-            eventTwo: this.two
-          }, options.hash);
-          this.normalizedHash = this.normalizeMethods(hash);
-        },
-        one: function() {}
-      });
-
-      this.hash = {
-        'eventOne': 'one',
-        'eventThree': 'three'
-      };
-
-      this.view = new this.View({
-        hash: this.hash
-      });
-    });
-
     it('should convert the strings that exist as functions to functions', function() {
-      expect(this.view.normalizedHash.eventOne).to.exist;
-      expect(this.view.normalizedHash.eventTwo).to.exist;
+      expect(this.normalizedHash).to.have.property('foo');
     });
+
     it('should ignore strings that dont exist as functions on the context', function() {
-      expect(this.view.normalizedHash.eventThree).to.be.undefined;
+      expect(this.normalizedHash).not.to.have.property('bar');
     });
   });
 });

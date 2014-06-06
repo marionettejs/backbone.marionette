a16d38424a5fc35075c98a27c1372b660f23e86b
Sam Saccone <samccone@gmail.com>
2014-06-05 22:21:37 -0400

    Improve spec reporter

--- Gruntfile.js
+++ Gruntfile.js
@@ -124,7 +124,7 @@ module.exports = function(grunt) {
       tests: {
         options: {
           require: 'spec/javascripts/setup/node.js',
-          reporter: 'dot',
+          reporter: grunt.option('mocha-reporter') || 'nyan',
           clearRequireCache: true,
           mocha: require('mocha')
         },

7ffdb0439d6540031a96f0bac049cfb288f9eb42
James Kyle <me@thejameskyle.com>
2014-06-28 22:28:40 -0400

    Cleaned up sortedViews spec

--- spec/javascripts/sortedViews.spec.js
+++ spec/javascripts/sortedViews.spec.js
@@ -1,218 +1,209 @@
-describe('collection/composite view sorting', function(){
+describe('collection/composite view sorting', function() {
   'use strict';
 
   beforeEach(function() {
-    // Shared View Definitions
-    // -----------------------
-
-    this.templateFn = _.template('composite template');
-
-    this.getCollectionChildren = function (el) {
-      return el.children().map(function(i, v){return v.innerHTML; }).get().join();
-    };
-
-    this.ChildView = Backbone.Marionette.ItemView.extend({
-      tagName: 'span',
-      render: function(){
-        this.$el.html(this.model.get('foo'));
-        this.trigger('render');
-      }
+    this.ChildView = Marionette.ItemView.extend({
+      template: _.template('<%= foo %>')
     });
 
-    this.CollectionView = Backbone.Marionette.CollectionView.extend({
+    this.CollectionView = Marionette.CollectionView.extend({
       childView: this.ChildView
     });
 
-    this.CompositeView = Backbone.Marionette.CompositeView.extend({
+    this.CompositeView = Marionette.CompositeView.extend({
       childView: this.ChildView,
-      template: this.templateFn
+      template: this.sinon.stub()
     });
+
+    this.collection = new Backbone.Collection([{foo: 1, bar: 4}, {foo: 2, bar: 3}, {foo: 3, bar: 2}]);
   });
 
-  describe('when working with collections with comparators', function(){
-    beforeEach(function(){
-      this.collection = new Backbone.Collection([{foo: 'abar', foo2: 'wbar'}, {foo: 'bbar', foo2: 'abar'}, {foo: 'wbar', foo2: 'bbar'}]);
+  describe('when working with collections with comparators', function() {
+    beforeEach(function() {
       this.collection.comparator = 'foo';
 
       this.collectionView = new this.CollectionView({
         childView: this.ChildView,
         collection: this.collection
       });
-      this.collectionView.render();
 
       this.compositeView = new this.CompositeView({
         childView: this.ChildView,
         collection: this.collection,
       });
+
+      this.collectionView.render();
       this.compositeView.render();
     });
 
-    describe('when adding a model', function(){
-      beforeEach(function(){
-        this.model = new Backbone.Model({foo: 'fbar', foo2: 'sbar'});
+    describe('when adding a model', function() {
+      beforeEach(function() {
+        this.model = new Backbone.Model({foo: 4, bar: 1});
         this.collection.add(this.model);
       });
 
-      it('should add the model to the list', function(){
-        expect(_.size(this.collectionView.children)).to.equal(4);
+      it('should add the model to the list', function() {
+        expect(this.collectionView.children).to.have.lengthOf(4);
       });
 
-      it('should have the order in the dom', function(){
-        expect(this.getCollectionChildren(this.collectionView.$el)).to.equal('abar,bbar,fbar,wbar');
-        expect(this.getCollectionChildren(this.compositeView.$el)).to.equal('abar,bbar,fbar,wbar');
+      it('should have the order in the dom', function() {
+        expect(this.collectionView.$el).to.have.$text('1234');
+        expect(this.compositeView.$el).to.have.$text('1234');
       });
     });
 
-    describe('when adding a model with the \'at\' option', function(){
-      beforeEach(function(){
-        this.model = new Backbone.Model({foo: 'fbar', foo2: 'sbar'});
+    describe('when adding a model with the "at" option', function() {
+      beforeEach(function() {
+        this.model = new Backbone.Model({foo: 0, bar: 1});
         this.collection.add(this.model, {at: 5});
       });
 
-      it('should add the model to the list', function(){
-        expect(_.size(this.collectionView.children)).to.equal(4);
+      it('should add the model to the list', function() {
+        expect(this.collectionView.children).to.have.lengthOf(4);
       });
 
-      it('should ignore the sorted order in the dom', function(){
-        expect(this.getCollectionChildren(this.collectionView.$el)).to.equal('abar,bbar,wbar,fbar');
-        expect(this.getCollectionChildren(this.compositeView.$el)).to.equal('abar,bbar,wbar,fbar');
+      it('should ignore the sorted order in the dom', function() {
+        expect(this.collectionView.$el).to.have.$text('1230');
+        expect(this.compositeView.$el).to.have.$text('1230');
       });
 
-      describe('and adding another', function(){
-        beforeEach(function(){
-          this.model = new Backbone.Model({foo: 'dbar', foo2: 'sbar'});
+      describe('and adding another', function() {
+        beforeEach(function() {
+          this.model = new Backbone.Model({foo: 5, bar: 0});
           this.collection.add(this.model);
         });
 
-        it('should render the sorted order in the dom', function(){
-          expect(this.getCollectionChildren(this.collectionView.$el)).to.equal('abar,bbar,dbar,fbar,wbar');
-          expect(this.getCollectionChildren(this.compositeView.$el)).to.equal('abar,bbar,dbar,fbar,wbar');
+        it('should render the sorted order in the dom', function() {
+          expect(this.collectionView.$el).to.have.$text('01235');
+          expect(this.compositeView.$el).to.have.$text('01235');
         });
       });
     });
 
-    describe('when silently adding a model', function(){
-      beforeEach(function(){
-        this.model = new Backbone.Model({foo: 'fbar', foo2: 'sbar'});
+    describe('when silently adding a model', function() {
+      beforeEach(function() {
+        this.model = new Backbone.Model({foo: 4, bar: 1});
         this.collection.add(this.model, {silent: true});
       });
 
-      it('shouldn\'t add the model to the list', function(){
-        expect(_.size(this.collectionView.children)).to.equal(3);
+      it('should not add the model to the list', function() {
+        expect(this.collectionView.children).to.have.lengthOf(3);
       });
 
-      it('should have the order in the dom', function(){
-        expect(this.getCollectionChildren(this.collectionView.$el)).to.equal('abar,bbar,wbar');
-        expect(this.getCollectionChildren(this.compositeView.$el)).to.equal('abar,bbar,wbar');
+      it('should have the order in the dom', function() {
+        expect(this.collectionView.$el).to.have.$text('123');
+        expect(this.compositeView.$el).to.have.$text('123');
       });
 
       describe('and then adding another', function () {
-        beforeEach(function(){
-          this.model = new Backbone.Model({foo: 'dbar', foo2: 'qbar'});
+        beforeEach(function() {
+          this.model = new Backbone.Model({foo: 5, bar: 0});
           this.collection.add(this.model);
+          this.collectionView.render();
+          this.compositeView.render();
         });
 
-        it('should add both models to the list', function(){
-          expect(_.size(this.collectionView.children)).to.equal(5);
+        it('should add both models to the list', function() {
+          expect(this.collectionView.children).to.have.lengthOf(5);
         });
 
-        it('should have the order in the dom', function(){
-          expect(this.getCollectionChildren(this.collectionView.$el)).to.equal('abar,bbar,dbar,fbar,wbar');
-          expect(this.getCollectionChildren(this.compositeView.$el)).to.equal('abar,bbar,dbar,fbar,wbar');
+        it('should have the order in the dom', function() {
+          expect(this.collectionView.$el).to.have.$text('12345');
+          expect(this.compositeView.$el).to.have.$text('12345');
         });
       });
     });
 
-    describe('when removing a model', function(){
-      beforeEach(function(){
+    describe('when removing a model', function() {
+      beforeEach(function() {
         this.model = this.collection.at(1);
         this.collection.remove(this.model);
       });
 
-      it('should have the order in the dom', function(){
-        expect(this.getCollectionChildren(this.collectionView.$el)).to.equal('abar,wbar');
-        expect(this.getCollectionChildren(this.compositeView.$el)).to.equal('abar,wbar');
+      it('should have the order in the dom', function() {
+        expect(this.collectionView.$el).to.have.$text('13');
+        expect(this.compositeView.$el).to.have.$text('13');
       });
 
-      describe('and then adding another', function(){
-        beforeEach(function(){
-          this.model = new Backbone.Model({foo: 'bbar'});
+      describe('and then adding another', function() {
+        beforeEach(function() {
+          this.model = new Backbone.Model({foo: '4'});
           this.sinon.spy(this.collectionView, 'render');
           this.collection.add(this.model);
         });
 
-        it('should have the order in the dom', function(){
-          expect(this.getCollectionChildren(this.collectionView.$el)).to.equal('abar,bbar,wbar');
-          expect(this.getCollectionChildren(this.compositeView.$el)).to.equal('abar,bbar,wbar');
+        it('should have the order in the dom', function() {
+          expect(this.collectionView.$el).to.have.$text('134');
+          expect(this.compositeView.$el).to.have.$text('134');
         });
 
-        it('should not call render', function(){
+        it('should not call render', function() {
           expect(this.collectionView.render.callCount).to.equal(0);
         });
       });
     });
 
-    describe('when changing the comparator', function(){
-      beforeEach(function(){
-        this.collection.comparator = 'foo2';
+    describe('when changing the comparator', function() {
+      beforeEach(function() {
+        this.collection.comparator = 'bar';
       });
 
-      describe('and triggering a sort', function(){
-        beforeEach(function(){
+      describe('and triggering a sort', function() {
+        beforeEach(function() {
           this.collection.sort();
         });
 
-        it('should have the order in the dom', function(){
-          expect(this.getCollectionChildren(this.collectionView.$el)).to.equal('bbar,wbar,abar');
-          expect(this.getCollectionChildren(this.compositeView.$el)).to.equal('bbar,wbar,abar');
+        it('should have the order in the dom', function() {
+          expect(this.collectionView.$el).to.have.$text('321');
+          expect(this.compositeView.$el).to.have.$text('321');
         });
       });
 
-      describe('and adding a new child', function(){
-        beforeEach(function(){
-          this.model = new Backbone.Model({foo: 'fbar', foo2: 'sbar'});
+      describe('and adding a new child', function() {
+        beforeEach(function() {
+          this.model = new Backbone.Model({foo: '4', bar: '4'});
           this.collection.add(this.model);
         });
 
-        it('should have the order in the dom', function(){
-          expect(this.getCollectionChildren(this.collectionView.$el)).to.equal('bbar,wbar,fbar,abar');
-          expect(this.getCollectionChildren(this.compositeView.$el)).to.equal('bbar,wbar,fbar,abar');
+        it('should have the order in the dom', function() {
+          expect(this.collectionView.$el).to.have.$text('3214');
+          expect(this.compositeView.$el).to.have.$text('3214');
         });
       });
     });
+  });
 
-    describe('when using \'{sort: false}\'', function(){
-      beforeEach(function(){
-        this.collection = new Backbone.Collection([{foo: 'abar', foo2: 'wbar'}, {foo: 'bbar', foo2: 'abar'}, {foo: 'wbar', foo2: 'bbar'}]);
-        this.collection.comparator = 'foo';
+  describe('when using `{sort: false}`', function() {
+    beforeEach(function() {
+      this.collectionView = new this.CollectionView({
+        childView: this.ChildView,
+        collection: this.collection,
+        sort: false
+      });
 
-        this.collectionView = new this.CollectionView({
-          childView: this.ChildView,
-          collection: this.collection,
-          sort: false
-        });
-        this.collectionView.render();
+      this.compositeView = new this.CompositeView({
+        childView: this.ChildView,
+        collection: this.collection,
+        sort: false
+      });
 
-        this.compositeView = new this.CompositeView({
-          childView: this.ChildView,
-          collection: this.collection,
-          sort: false
-        });
-        this.compositeView.render();
+      this.collectionView.render();
+      this.compositeView.render();
+    });
 
-        this.model = new Backbone.Model({foo: 'fbar', foo2: 'sbar'});
+    describe('and adding a model', function() {
+      beforeEach(function() {
+        this.model = new Backbone.Model({foo: '0', bar: '5'});
         this.collection.add(this.model);
       });
 
-      describe('and adding a model', function(){
-        it('should add the model to the list', function(){
-          expect(_.size(this.collectionView.children)).to.equal(4);
-        });
+      it('should add the model to the list', function() {
+        expect(this.collectionView.children).to.have.lengthOf(4);
+      });
 
-        it('should have the order in the dom', function(){
-          expect(this.getCollectionChildren(this.collectionView.$el)).to.equal('abar,bbar,wbar,fbar');
-          expect(this.getCollectionChildren(this.compositeView.$el)).to.equal('abar,bbar,wbar,fbar');
-        });
+      it('should have the order in the dom', function() {
+        expect(this.collectionView.$el).to.have.$text('1230');
+        expect(this.compositeView.$el).to.have.$text('1230');
       });
     });
   });

a0cf4d3e08919d30bd0f990d7135837c9447d96a
Paul Falgout <paul.falgout@roundingwell.com>
2014-08-20 23:15:28 -0500

    Include View cid in ViewDestroyError
    And normalize error throwing using throwError helper

--- src/marionette.view.js
+++ src/marionette.view.js
@@ -159,9 +159,9 @@ Marionette.View = Backbone.View.extend({
   // Internal helper method to verify whether the view hasn't been destroyed
   _ensureViewIsIntact: function() {
     if (this.isDestroyed) {
-      var err = new Error('Cannot use a view thats already been destroyed.');
-      err.name = 'ViewDestroyedError';
-      throw err;
+      throwError('View (cid: "' + this.cid +
+          '") has already been destroyed and cannot be used.',
+          'ViewDestroyedError');
     }
   },
 

c3e63940e212758b007dffa50d698370cbd96c5c
Jason Laster <jason.laster.11@gmail.com>
2014-08-30 16:27:12 -0400

    Add markdown parsing to examples and descriptions

--- tasks/jsDocFile.js
+++ tasks/jsDocFile.js
@@ -7,7 +7,9 @@
 
 var dox = require('dox');
 var _ = require('underscore');
-var yaml = require('js-yaml')
+var yaml = require('js-yaml');
+var marked = require('marked');
+
 
 
 function JsDocFilesTask(grunt) {
@@ -26,15 +28,17 @@ _.extend(JsDocFilesTask.prototype, {
         }.bind(this));
     }.bind(this));
   },
-  
+
   compileJsDoc: function(file, filepath) {
-    
+
     var doc = this.grunt.file.read(filepath);
     var json = this.parseYaml(doc);
 
     json.functions = json.functions || [];
     json.properties = json.properties || [];
     json.constructor = json.constructor || "";
+
+    json.description = this.parseDescription(json.description);
 
     _.each(json.functions, function(value, name) {
       json.functions[name] = this.parseBody(value, name);
@@ -43,29 +47,34 @@ _.extend(JsDocFilesTask.prototype, {
     _.each(json.properties, function(value, name) {
       json.properties[name] = this.parseBody(value, name);
     }, this);
-        
+
     json.constructor = this.parseBody(json.constructor, 'constructor');
 
     this.writeJSON(file, json);
   },
-  
+
+  parseDescription: function(description) {
+    description = description || "";
+    return marked(description);
+  },
+
   /**
-   * parseBody takes a yaml body, which is either a string or an object. 
+   * parseBody takes a yaml body, which is either a string or an object.
    * if it's just a string, it assumes it's one dox string, if it's an object it
    * assumes it has a description and examples.
    *
    *
-   * e.g 
+   * e.g
    * foo: |
    *    Foo function is yay
-   *    
+   *
    *     @param wtf
    *
    * or...
    * foo:
    *   description: |
    *     Foo function is yay
-   *     
+   *
    *      @param wtf
    *
    *   examples:
@@ -77,13 +86,13 @@ _.extend(JsDocFilesTask.prototype, {
    *             foo(1,2,3)
    *          ```
    *
-  **/  
+  **/
   parseBody: function(value, name) {
     var result = {};
-    
+
     if (_.isEmpty(value)) {
       return result;
-    }    
+    }
 
     // Function values have both a description and examples
     // If the function value is a string, only the description was added
@@ -95,9 +104,30 @@ _.extend(JsDocFilesTask.prototype, {
     }
 
     result.description = this.parseDox(result.description, name);
+
+    _.each(result.examples, function(example) {
+      result.examples[name] = this.parseExample(example);
+    }, this);
+
+
     return result;
   },
-  
+
+  parseExample: function(example) {
+    if (!example.name) {
+        this.grunt.fail.fatal('jsDocFile failed to find example name');
+    }
+
+    if (!example.example) {
+        this.grunt.fail.fatal('jsDocFile failed to find example for ' + example.name);
+    }
+
+    example.example = marked(example.example);
+
+    return example;
+  },
+
+
   /**
    * parse the dox comment string.
    * We also pluck some tags from the tags property to make it easy to access later.
@@ -105,35 +135,35 @@ _.extend(JsDocFilesTask.prototype, {
    *
   **/
   parseDox: function(docString, name) {
-    
+
     try {
       var doc = dox.parseComment(docString);
-    } catch (err) {      
+    } catch (err) {
       this.grunt.fail.fatal('jsDocFile failed to parse dox at ' + name);
     }
 
     var tags = doc.tags || [];
     doc.api = _.findWhere(tags, {type: 'api'});
     doc.params = _.where(tags, {type: 'param'});
-    
+
     return doc;
   },
-  
+
   // write parsed api to file
   writeJSON: function(file, json) {
     var prettyJSON = JSON.stringify(json, undefined, 2);
     this.grunt.file.write(file.dest, prettyJSON);
   },
-  
+
   // read yaml file
   parseYaml: function(file) {
     try {
       return yaml.safeLoad(file);
-    } catch (err){ 
+    } catch (err){
       this.grunt.fail.fatal(err.name + ":\n"+  err.reason + "\n\n" + err.mark);
     }
   }
-  
+
 });
 
 module.exports = function(grunt) {

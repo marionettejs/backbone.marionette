cc902fa1dc1825fbf62b4428683b6006a32c2a71
Jmeas <jellyes2@gmail.com>
2014-09-13 13:48:58 -0400

    Aliases Marionette as Mn

--- src/build/bundled.js
+++ src/build/bundled.js
@@ -3,14 +3,14 @@
   /* istanbul ignore next */
   if (typeof define === 'function' && define.amd) {
     define(['backbone', 'underscore'], function(Backbone, _) {
-      return (root.Marionette = factory(root, Backbone, _));
+      return (root.Marionette = root.Mn = factory(root, Backbone, _));
     });
   } else if (typeof exports !== 'undefined') {
     var Backbone = require('backbone');
     var _ = require('underscore');
     module.exports = factory(root, Backbone, _);
   } else {
-    root.Marionette = factory(root, root.Backbone, root._);
+    root.Marionette = root.Mn = factory(root, root.Backbone, root._);
   }
 
 }(this, function(root, Backbone, _) {

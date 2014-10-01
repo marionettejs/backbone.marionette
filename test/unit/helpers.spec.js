bb1e40767d47f7c9dce6504528a9a69966d4024f
Jmeas <jellyes2@gmail.com>
2014-10-01 00:24:45 -0400

    Adds isNodeAttached helper method.

--- test/unit/helpers.spec.js
+++ test/unit/helpers.spec.js
@@ -1,3 +1,13 @@
+describe("isNodeAttached", function() {
+  'use strict';
+
+  describe("document.documentElement", function() {
+    it("should exist", function() {
+      expect(document.documentElement).to.not.be.undefined;
+    });
+  });
+});
+
 describe("normalizeUIKeys", function () {
   'use strict';
 

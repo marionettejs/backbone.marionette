3204394077ac4ddb00423c1d6216429f7ba1af81
megawac <megawac@gmail.com>
2014-10-22 23:56:11 -0400

    Optimize view.triggerMethod

--- src/trigger-method.js
+++ src/trigger-method.js
@@ -1,15 +1,10 @@
+/* jshint maxstatements: 14, maxcomplexity: 7 */
+
 // Trigger Method
 // --------------
 
-// Trigger an event and/or a corresponding method name. Examples:
-//
-// `this.triggerMethod("foo")` will trigger the "foo" event and
-// call the "onFoo" method.
-//
-// `this.triggerMethod("foo:bar")` will trigger the "foo:bar" event and
-// call the "onFooBar" method.
-Marionette.triggerMethod = function(event) {
 
+Marionette._triggerMethod = (function() {
   // split the event name on the ":"
   var splitter = /(^|:)(\w)/gi;
 
@@ -19,23 +14,46 @@ Marionette.triggerMethod = function(event) {
     return eventName.toUpperCase();
   }
 
-  // get the method name from the event name
-  var methodName = 'on' + event.replace(splitter, getEventName);
-  var method = this[methodName];
-  var result;
+  return function(context, event, args) {
+    var noEventArg = arguments.length < 3;
+    if (noEventArg) {
+      args = event;
+      event = args[0];
+    }
 
-  // call the onMethodName if it exists
-  if (_.isFunction(method)) {
-    // pass all arguments, except the event name
-    result = method.apply(this, _.tail(arguments));
-  }
+    // get the method name from the event name
+    var methodName = 'on' + event.replace(splitter, getEventName);
+    var method = context[methodName];
+    var result;
 
-  // trigger the event, if a trigger method exists
-  if (_.isFunction(this.trigger)) {
-    this.trigger.apply(this, arguments);
-  }
+    // call the onMethodName if it exists
+    if (_.isFunction(method)) {
+      // pass all args, except the event name
+      result = method.apply(context, noEventArg ? _.rest(args) : args);
+    }
 
-  return result;
+    // trigger the event, if a trigger method exists
+    if (_.isFunction(context.trigger)) {
+      if (noEventArg + args.length > 1) {
+        context.trigger.apply(context, noEventArg ? args : [event].concat(_.rest(args, 0)));
+      } else {
+        context.trigger(event);
+      }
+    }
+
+    return result;
+  };
+})();
+
+// Trigger an event and/or a corresponding method name. Examples:
+//
+// `this.triggerMethod("foo")` will trigger the "foo" event and
+// call the "onFoo" method.
+//
+// `this.triggerMethod("foo:bar")` will trigger the "foo:bar" event and
+// call the "onFooBar" method.
+Marionette.triggerMethod = function(event) {
+  return Marionette._triggerMethod(this, arguments);
 };
 
 // triggerMethodOn invokes triggerMethod on a specific context

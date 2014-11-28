5345ad6dad9221c1b4a5b4ebb3e8fa4d0346f375
Graeme <megawac@gmail.com>
2014-11-27 20:45:28 -0500

    :100:% test coverage of serialize data

--- test/unit/item-view.spec.js
+++ test/unit/item-view.spec.js
@@ -310,11 +310,15 @@ describe('item view', function() {
     describe('and the view only has a collection', function() {
       beforeEach(function() {
         this.itemView.collection = new Backbone.Collection(this.collectionData);
-        this.itemView.serializeData();
+        this.itemView.serializeData(1, 2, 3);
       });
 
       it("should call serializeCollection", function(){
         expect(this.itemView.serializeCollection).to.have.been.calledOnce;
+      });
+
+      it('and the serialize function should be called with the provided arguments', function() {
+        expect(this.itemView.serializeCollection).to.have.been.calledWith(this.itemView.collection, 1, 2, 3);
       });
 
       it("should not call serializeModel", function() {
@@ -326,11 +330,15 @@ describe('item view', function() {
       beforeEach(function() {
         this.itemView.model = new Backbone.Model(this.modelData);
         this.itemView.collection = new Backbone.Collection(this.collectionData);
-        this.itemView.serializeData();
+        this.itemView.serializeData(1, 2, 3);
       });
 
       it("should call serializeModel", function() {
         expect(this.itemView.serializeModel).to.have.been.calledOnce;
+      });
+
+      it('and the serialize function should be called with the provided arguments', function() {
+        expect(this.itemView.serializeModel).to.have.been.calledWith(this.itemView.model, 1, 2, 3);
       });
 
       it('should not call serializeCollection', function() {

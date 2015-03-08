9d571f42b3e690dd84d9fc05ad4ebb81ac8acb99
Sam Saccone <samccone@gmail.com>
2015-03-08 16:40:20 -0400

    Update controller to be inline with Object.
    Since they were around 95% the same, I have gone ahead and cut down on
    duplicate code and simplified them.
    
    There was one test change needed, since the controller options used to default to an
    empty object, the old test asserting that it was invoked with an empty
    object is no longer valid.
    
    This should not be breaking and so I think it we are fine to go ahead with this.

--- src/controller.js
+++ src/controller.js
@@ -4,21 +4,8 @@
 // A multi-purpose object to use as a controller for
 // modules and routers, and as a mediator for workflow
 // and coordination of other objects, views, and more.
-Marionette.Controller = function(options) {
-  this.options = options || {};
-
-  if (_.isFunction(this.initialize)) {
-    this.initialize(this.options);
-  }
-};
-
-Marionette.Controller.extend = Marionette.extend;
-
-// Controller Methods
-// --------------
-
-// Ensure it can trigger events with Backbone.Events
-_.extend(Marionette.Controller.prototype, Backbone.Events, {
+Marionette.Controller = Marionette.Object.extend({
+  options: {},
   destroy: function() {
     Marionette._triggerMethod(this, 'before:destroy', arguments);
     Marionette._triggerMethod(this, 'destroy', arguments);
@@ -26,16 +13,5 @@ _.extend(Marionette.Controller.prototype, Backbone.Events, {
     this.stopListening();
     this.off();
     return this;
-  },
-
-  // import the `triggerMethod` to trigger events with corresponding
-  // methods if the method exists
-  triggerMethod: Marionette.triggerMethod,
-
-  // A handy way to merge options onto the instance
-  mergeOptions: Marionette.mergeOptions,
-
-  // Proxy `getOption` to enable getting options from this or this.options by name.
-  getOption: Marionette.proxyGetOption
-
+  }
 });

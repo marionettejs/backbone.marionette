159b3eb362269998d7165117e12e981c55fd30bd
Joseph Orbegoso Pea <joe@trusktr.io>
2015-03-20 21:50:27 -0700

    Convert error.js. TODO: Handle Marionette.VERSION.

--- src/error.js
+++ src/error.js
@@ -1,38 +1,46 @@
 // Error
 // -----
 
+import _       from 'underscore';
+import helpers from './helpers';
+
 var errorProps = ['description', 'fileName', 'lineNumber', 'name', 'message', 'number'];
 
-Marionette.Error = Marionette.extend.call(Error, {
-  urlRoot: 'http://marionettejs.com/docs/v' + Marionette.VERSION + '/',
+var exports = {
+  Error: helpers.extend.call(Error, {
+    urlRoot: 'http://marionettejs.com/docs/v' + Marionette.VERSION + '/', // trusktr TODO: handle VERSION.
 
-  constructor: function(message, options) {
-    if (_.isObject(message)) {
-      options = message;
-      message = options.message;
-    } else if (!options) {
-      options = {};
+    constructor: function(message, options) {
+      if (_.isObject(message)) {
+        options = message;
+        message = options.message;
+      } else if (!options) {
+        options = {};
+      }
+
+      var error = Error.call(this, message);
+      _.extend(this, _.pick(error, errorProps), _.pick(options, errorProps));
+
+      this.captureStackTrace();
+
+      if (options.url) {
+        this.url = this.urlRoot + options.url;
+      }
+    },
+
+    captureStackTrace: function() {
+      if (Error.captureStackTrace) {
+        Error.captureStackTrace(this, exports.Error);
+      }
+    },
+
+    toString: function() {
+      return this.name + ': ' + this.message + (this.url ? ' See: ' + this.url : '');
     }
+  })
 
-    var error = Error.call(this, message);
-    _.extend(this, _.pick(error, errorProps), _.pick(options, errorProps));
+};
 
-    this.captureStackTrace();
+exports.Error.extend = helpers.extend;
 
-    if (options.url) {
-      this.url = this.urlRoot + options.url;
-    }
-  },
-
-  captureStackTrace: function() {
-    if (Error.captureStackTrace) {
-      Error.captureStackTrace(this, Marionette.Error);
-    }
-  },
-
-  toString: function() {
-    return this.name + ': ' + this.message + (this.url ? ' See: ' + this.url : '');
-  }
-});
-
-Marionette.Error.extend = Marionette.extend;
+export default exports;

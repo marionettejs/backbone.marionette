cd87192c21358bbaeeda4eea892f440b9bef71bf
Joseph Orbegoso Pea <joe@trusktr.io>
2015-03-20 17:28:30 -0700

    Convert dom-refresh.js

--- src/dom-refresh.js
+++ src/dom-refresh.js
@@ -1,36 +1,42 @@
 // DOM Refresh
 // -----------
 
-// Monitor a view's state, and after it has been rendered and shown
-// in the DOM, trigger a "dom:refresh" event every time it is
-// re-rendered.
+var _       = require('underscore');
+var helpers = require('./helpers.js');
 
-Marionette.MonitorDOMRefresh = function(view) {
+exports = {
 
-  // track when the view has been shown in the DOM,
-  // using a Marionette.Region (or by other means of triggering "show")
-  function handleShow() {
-    view._isShown = true;
-    triggerDOMRefresh();
-  }
+  // Monitor a view's state, and after it has been rendered and shown
+  // in the DOM, trigger a "dom:refresh" event every time it is
+  // re-rendered.
 
-  // track when the view has been rendered
-  function handleRender() {
-    view._isRendered = true;
-    triggerDOMRefresh();
-  }
+  MonitorDOMRefresh: function(view) {
 
-  // Trigger the "dom:refresh" event and corresponding "onDomRefresh" method
-  function triggerDOMRefresh() {
-    if (view._isShown && view._isRendered && Marionette.isNodeAttached(view.el)) {
-      if (_.isFunction(view.triggerMethod)) {
-        view.triggerMethod('dom:refresh');
+    // track when the view has been shown in the DOM,
+    // using a Marionette.Region (or by other means of triggering "show")
+    function handleShow() {
+      view._isShown = true;
+      triggerDOMRefresh();
+    }
+
+    // track when the view has been rendered
+    function handleRender() {
+      view._isRendered = true;
+      triggerDOMRefresh();
+    }
+
+    // Trigger the "dom:refresh" event and corresponding "onDomRefresh" method
+    function triggerDOMRefresh() {
+      if (view._isShown && view._isRendered && helpers.isNodeAttached(view.el)) {
+        if (_.isFunction(view.triggerMethod)) {
+          view.triggerMethod('dom:refresh');
+        }
       }
     }
-  }
 
-  view.on({
-    show: handleShow,
-    render: handleRender
-  });
+    view.on({
+      show: handleShow,
+      render: handleRender
+    });
+  }
 };

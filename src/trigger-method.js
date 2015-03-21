93ae78dadcc944299aef3a8343c63f39f43a6ec8
Joseph Orbegoso Pea <joe@trusktr.io>
2015-03-20 17:25:52 -0700

    Convert trigger-method.js.

--- src/trigger-method.js
+++ src/trigger-method.js
@@ -3,7 +3,9 @@
 // Trigger Method
 // --------------
 
-Marionette._triggerMethod = (function() {
+import _ from 'underscore';
+
+var _triggerMethod = (function() {
   // split the event name on the ":"
   var splitter = /(^|:)(\w)/gi;
 
@@ -13,7 +15,7 @@ Marionette._triggerMethod = (function() {
     return eventName.toUpperCase();
   }
 
-  return function(context, event, args) {
+  return function _triggerMethod(context, event, args) {
     var noEventArg = arguments.length < 3;
     if (noEventArg) {
       args = event;
@@ -47,21 +49,21 @@ Marionette._triggerMethod = (function() {
 //
 // `this.triggerMethod("foo:bar")` will trigger the "foo:bar" event and
 // call the "onFooBar" method.
-Marionette.triggerMethod = function(event) {
-  return Marionette._triggerMethod(this, arguments);
-};
+function triggerMethod(event) {
+  return _triggerMethod(this, arguments);
+}
 
 // triggerMethodOn invokes triggerMethod on a specific context
 //
 // e.g. `Marionette.triggerMethodOn(view, 'show')`
 // will trigger a "show" event or invoke onShow the view.
-Marionette.triggerMethodOn = function(context) {
+function triggerMethodOn(context) {
   var fnc = _.isFunction(context.triggerMethod) ?
                 context.triggerMethod :
-                Marionette.triggerMethod;
+                triggerMethod;
 
   return fnc.apply(context, _.rest(arguments));
-};
+}
 
 // triggerMethodMany invokes triggerMethod on many targets from a source
 // it's useful for standardizing a pattern where we propogate an event from a source
@@ -70,10 +72,18 @@ Marionette.triggerMethodOn = function(context) {
 // For each target we want to follow the pattern
 // target.triggerMethod(event, target, source, ...other args)
 // e.g childview.triggerMethod('attach', childView, region, ...args)
-Marionette.triggerMethodMany = function(targets, source, eventName) {
+function triggerMethodMany(targets, source, eventName) {
   var args = _.drop(arguments, 3);
 
   _.each(targets, function(target) {
     Marionette.triggerMethodOn.apply(target, [target, eventName, target, source].concat(args));
   });
+}
+
+export default {
+  _triggerMethod:     _triggerMethod,
+  triggerMethod:      triggerMethod,
+  triggerMethodOn:    triggerMethodOn,
+  triggerMethodMany:  triggerMethodMany
 };
+

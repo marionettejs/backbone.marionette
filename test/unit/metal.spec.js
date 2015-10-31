41aa89aadd018457a7ca190ca1bab8d9e6cb0470
Paul Falgout <paul@otterball.com>
2015-10-31 15:28:41 -0500

    Fix metal for travis
    Travis installing other backbone versions isn't attaching metal correctly.

--- test/unit/metal.spec.js
+++ test/unit/metal.spec.js
@@ -3,25 +3,25 @@ describe('metal', function() {
     it('view instances are instances of classes', function() {
       expect(new Marionette.View()).to.be.instanceof(Backbone.View);
       expect(new Marionette.View()).to.be.instanceof(Marionette.View);
-      expect(new Marionette.View()).to.be.instanceof(Backbone.Metal.Class);
+      expect(new Marionette.View()).to.be.instanceof(Metal.Class);
     });
 
     it('model instances are instances of classes', function() {
       expect(new Marionette.Model()).to.be.instanceof(Backbone.Model);
       expect(new Marionette.Model()).to.be.instanceof(Marionette.Model);
-      expect(new Marionette.View()).to.be.instanceof(Backbone.Metal.Class);
+      expect(new Marionette.View()).to.be.instanceof(Metal.Class);
     });
 
     it('collection instances are instances of classes', function() {
       expect(new Marionette.Collection()).to.be.instanceof(Backbone.Collection);
       expect(new Marionette.Collection()).to.be.instanceof(Marionette.Collection);
-      expect(new Marionette.View()).to.be.instanceof(Backbone.Metal.Class);
+      expect(new Marionette.View()).to.be.instanceof(Metal.Class);
     });
 
     it('router instances are instances of classes', function() {
       expect(new Marionette.Router()).to.be.instanceof(Backbone.Router);
       expect(new Marionette.Router()).to.be.instanceof(Marionette.Router);
-      expect(new Marionette.View()).to.be.instanceof(Backbone.Metal.Class);
+      expect(new Marionette.View()).to.be.instanceof(Metal.Class);
     });
   });
 });

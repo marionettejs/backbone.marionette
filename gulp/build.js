f545d5e3a2b58ced0ece37ae08d250fb5ebe4877
Paul Falgout <paul@otterball.com>
2015-11-29 16:13:03 -0600

    Update gulp build for ES6
    This was a pain.
    There's currently no babel 6 solution with sane output.
    
    Rollup works find with babel 5 and the output is readable (and thus annotatable)
    
    The whitelist was just simply ignored.  I'm not sure it works for everything or there is undocumented dependencies.  Anyhow, I don't think it is needed with rollup.

--- gulp/build.js
+++ gulp/build.js
@@ -1,41 +1,108 @@
-import gulp from 'gulp';
-import header from 'gulp-header';
-import preprocess from 'gulp-preprocess';
-import rename from 'gulp-rename';
+import gulp       from 'gulp';
+import header     from 'gulp-header';
+import plumber    from 'gulp-plumber';
+import file       from 'gulp-file';
+import filter     from 'gulp-filter';
+import rename     from 'gulp-rename';
 import sourcemaps from 'gulp-sourcemaps';
-import uglify from 'gulp-uglify';
-import unwrapper from 'gulp-unwrapper';
+import tap        from 'gulp-tap';
+import uglify     from 'gulp-uglify';
+import unwrapper  from 'gulp-unwrapper';
 
-import banner from  './_banner';
-import {name} from '../package.json';
+import { rollup } from 'rollup';
+import babel      from 'rollup-plugin-babel';
+import json       from 'rollup-plugin-json';
 
-// NOTE: This is the ES5 builder
+import banner     from  './_banner';
+import {name, version} from '../package.json';
 
-const srcPath = './src/build/';
-const buildPath = './lib/';
+const srcPath = 'src/';
+const buildPath = 'lib/';
 
 const unwrapConfig = [
   './node_modules/backbone.babysitter/lib/backbone.babysitter.js',
   './node_modules/backbone.radio/build/backbone.radio.js',
 ];
 
+let unwrappedBundledLibs = '';
+
 gulp.task('unwrap', function(){
   return gulp.src(unwrapConfig)
     .pipe(unwrapper())
-    .pipe(gulp.dest('./tmp/'));
+    .pipe(tap(file => {
+      // save the file contents in memory
+      unwrappedBundledLibs += file.contents.toString();
+    }));
 });
 
-function buildLib(type, dest){
-  return gulp.src(srcPath + type + '.js')
-    .pipe(preprocess())
-    .pipe(header(banner[type]))
-    .pipe(rename(dest + name + '.js'))
-    .pipe(gulp.dest(''))
-    .pipe(uglify())
-    .pipe(header(banner[type]))
-    .pipe(rename(dest + name + '.min.js'))
-    .pipe(sourcemaps.write('./'))
-    .pipe(gulp.dest('./'));
+const attachBundles = `
+
+// Attach bundled libraries to expected local vars
+Radio = Backbone.Radio;
+ChildViewContainer = Backbone.ChildViewContainer;
+`;
+
+function _generate(bundle, type){
+  let intro = '';
+
+  // adds unwrapped  bundled libs
+  if(type === 'bundled'){
+    intro = unwrappedBundledLibs + attachBundles;
+  }
+
+  return bundle.generate({
+    format: 'umd',
+    moduleName: 'Marionette  = global.Mn',
+    sourceMap: true,
+    banner: banner[type],
+    intro: intro,
+    globals: {
+      'backbone': 'Backbone',
+      'underscore': '_',
+      'backbone.babysitter': 'Backbone.ChildViewContainer',
+      'backbone.radio': 'Backbone.Radio'
+    }
+  });
+}
+
+function bundle(type) {
+  return rollup({
+    entry: srcPath + name + '.js',
+    external: ['underscore', 'backbone', 'backbone.babysitter', 'backbone.radio'],
+    plugins: [
+      json(),
+      babel({
+        sourceMaps: true
+      })
+    ]
+  }).then(bundle => {
+    return _generate(bundle, type);
+  }).then(gen => {
+    gen.code += '\n//# sourceMappingURL=' + gen.map.toUrl();
+    return gen;
+  });
+}
+
+function buildLib(type, dest) {
+  return bundle(type).then(gen => {
+    return file(name + '.js', gen.code, {src: true})
+      .on('error', function(err) {
+        console.log(err);
+        this.emit('end');
+      })
+      .pipe(plumber())
+      .pipe(sourcemaps.init({loadMaps: true}))
+      .pipe(sourcemaps.write('./'))
+      .pipe(gulp.dest(dest))
+      .pipe(filter(['*', '!**/*.js.map']))
+      .pipe(rename(name + '.min.js'))
+      .pipe(sourcemaps.init({loadMaps: true}))
+      .pipe(uglify({
+        preserveComments: 'license'
+      }))
+      .pipe(sourcemaps.write('./'))
+      .pipe(gulp.dest(dest));
+  });
 }
 
 function buildCore(){
@@ -51,3 +118,4 @@ gulp.task('build-core', ['lint-src'], buildCore);
 gulp.task('build-bundled', ['lint-src', 'unwrap'], buildBundled);
 
 gulp.task('build', ['build-core', 'build-bundled']);
+

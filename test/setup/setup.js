318da0614d11d736243362c9d2900d1b0cec0c41
denar90 <denysov.artem@gmail.com>
2016-08-14 17:43:24 +0300

    add jQuery to window

--- test/setup/setup.js
+++ test/setup/setup.js
@@ -11,7 +11,7 @@ module.exports = function() {
 
   ChildViewContainer = 'default' in ChildViewContainer ? ChildViewContainer.default : ChildViewContainer;
 
-  global.$ = global.jQuery = jQuery;
+  global.window.jQuery = global.window.$ = global.$ = global.jQuery = jQuery;
   global._ = _;
   global.Backbone = Backbone;
   global.Marionette = Backbone.Marionette = Marionette;

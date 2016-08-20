2307b0791888764df63de31399aa357ff59d2abc
Rafael De Leon <rafaelideleon@gmail.com>
2016-08-20 13:47:13 -0700

    CollectionView
    addChildView
    * using `_setupChildView`
    * triggers `event~before:add:child`
    * triggers `event~add:child`
    * when buffering, will add view to list of children
      but will not update children length for performance increase
    * using `_renderView`
    
    ~~_addChildView~~
    
    _updateIndices
    * removed need to pass index
    
    _addChild
    * removed passing ChildView param
    * using `_createView`
    
    _onCollectionAdd
    * using `_addChild`
    
    _showCollection
    * using `_addChild`
    * calls `children._updateLength`
    
    _showEmptyView
    * removed unnecessary `view._parent` setting

--- src/collection-view.js
+++ src/collection-view.js
@@ -122,8 +122,7 @@ const CollectionView = Backbone.View.extend({
 
     if (this._shouldAddChild(child, index)) {
       this._destroyEmptyView();
-      const ChildView = this._getChildView(child);
-      this._addChild(child, ChildView, index);
+      this._addChild(child, index)
     }
   },
 
@@ -402,28 +401,10 @@ const CollectionView = Backbone.View.extend({
     }
   },
 
-  _renderChildView(view) {
-    if (!view.supportsRenderLifecycle) {
-      triggerMethodOn(view, 'before:render', view);
-    }
-
-    // Render view
-    view.render();
-
-    if (!view.supportsRenderLifecycle) {
-      view._isRendered = true;
-      triggerMethodOn(view, 'render', view);
-    }
-
-    return view;
-  },
-
   // Internal method to loop through collection and show each child view.
   _showCollection(models) {
-    _.each(models, (child, index) => {
-      const ChildView = this._getChildView(child);
-      this._addChild(child, ChildView, index);
-    });
+    _.each(models, _.bind(this._addChild, this));
+    this.children._updateLength();
   },
 
   // Allow the collection to be sorted by a custom view comparator
@@ -497,10 +478,8 @@ const CollectionView = Backbone.View.extend({
       const view = this.buildChildView(model, EmptyView, emptyViewOptions);
 
       this.triggerMethod('before:render:empty', this, view);
-      this._addChildView(view, 0);
+      this.addChildView(view, 0);
       this.triggerMethod('render:empty', this, view);
-
-      view._parent = this;
     }
   },
 
@@ -563,11 +542,8 @@ const CollectionView = Backbone.View.extend({
   },
 
   // Internal method for building and adding a child view
-  _addChild(child, ChildView, index) {
-    const childViewOptions = this._getChildViewOptions(child, index);
-
-    const view = this.buildChildView(child, ChildView, childViewOptions);
-
+  _addChild(child, index) {
+    const view = this._createView(child, index);
     this.addChildView(view, index);
 
     return view;
@@ -586,13 +562,21 @@ const CollectionView = Backbone.View.extend({
   // children in sync with the collection.
   addChildView(view, index) {
     this.triggerMethod('before:add:child', this, view);
+    this._setupChildView(view, index);
 
-    // increment indices of views after this one
-    this._updateIndices(view, true, index);
+    // Store the child view itself so we can properly remove and/or destroy it later
+    if (this._isBuffering) {
+      // Add to children, but don't update children's length.
+      this.children._add(view);
+    } else {
+      // increment indices of views after this one
+      this._updateIndices(view, true);
+      this.children.add(view);
+    }
 
-    view._parent = this;
+    this._renderView(view);
 
-    this._addChildView(view, index);
+    this._attachView(view, index);
 
     this.triggerMethod('add:child', this, view);
 
@@ -601,17 +585,12 @@ const CollectionView = Backbone.View.extend({
 
   // Internal method. This decrements or increments the indices of views after the added/removed
   // view to keep in sync with the collection.
-  _updateIndices(views, increment, index) {
+  _updateIndices(views, increment) {
     if (!this.sort) {
       return;
     }
 
     const view = _.isArray(views) ? this._findGreatestIndexedView(views) : views;
-
-    if (increment) {
-      // assign the index to the view
-      view._index = index;
-    }
 
     // update the indexes of views after this one
     this.children.each((laterView) => {
@@ -619,21 +598,6 @@ const CollectionView = Backbone.View.extend({
         laterView._index += increment ? 1 : -1;
       }
     });
-  },
-
-  // Internal Method. Add the view to children and render it at the given index.
-  _addChildView(view, index) {
-    monitorViewEvents(view);
-
-    // set up the child view event forwarding
-    this._proxyChildEvents(view);
-
-    // Store the child view itself so we can properly remove and/or destroy it later
-    this.children.add(view);
-
-    this._renderView(view);
-
-    this._attachView(view, index);
   },
 
   _renderView(view) {

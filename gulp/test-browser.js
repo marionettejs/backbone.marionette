04e7f754e8f997a8f1f139a47bd92aa7e4d78de1
denar90 <denysov.artem@gmail.com>
2016-10-22 20:30:13 +0300

    update cross browser testing

--- gulp/test-browser.js
+++ gulp/test-browser.js
@@ -1,5 +1,5 @@
 import gulp from 'gulp';
-import livereload from 'gulp-livereload';
+import easySauce from 'easy-sauce';
 
 import { rollup } from 'rollup';
 import babel from 'rollup-plugin-babel';
@@ -9,6 +9,23 @@ import nodeGlobals from 'rollup-plugin-node-globals';
 import commonjs from 'rollup-plugin-commonjs';
 import json from 'rollup-plugin-json';
 import opn from 'opn';
+
+const sauceConf = {
+  name: 'Marionette.js',
+  username: process.env.SAUCE_USERNAME,
+  key: process.env.SAUCE_ACCESS_KEY,
+  tests: '/test/runner.html',
+  port: '8080',
+  framework: 'mocha',
+  platforms: [
+    ['Windows 10', 'MicrosoftEdge', 'latest'],
+    ['Windows 10', 'chrome', 'latest'],
+    ['OS X 10.11', 'chrome', 'latest'],
+    ['OS X 10.11', 'firefox', "latest"],
+    ['OS X 10.11', 'safari', '9.0']
+  ],
+  service: 'sauce-connect'
+};
 
 function bundle() {
   return rollup({
@@ -33,15 +50,41 @@ function bundle() {
       moduleName: 'MnTests',
       dest: './tmp/__spec-build.js'
     });
-  }).then(livereload.changed('./tmp/__spec-build.js'));
+  });
 }
 
 function browserWatch() {
-  livereload.listen({port: 35729, host: 'localhost', start: true});
   opn('./test/runner.html');
   gulp.watch(['src/**/*.js', 'test/**/*.js'], ['browser-bundle']);
+}
+
+function sauceRunner() {
+  easySauce(sauceConf)
+    .on('message', function(message) {
+      // A message has been emitted, inform the user.
+      console.log(message);
+    })
+    .on('update', function(job) {
+      // A job's status has been updated
+      console.log(job.status);
+    })
+    .on('done', function(passed, jobs) {
+      // All tests have completed!
+      if (passed) {
+        console.log('All tests passed!');
+      }
+      else {
+        console.log('Oops, there were failures:\n' + jobs);
+      }
+    })
+    .on('error', function(err) {
+      // An error occurred at some point running the tests.
+      throw new Error(err.message);
+    });
 }
 
 gulp.task('browser-bundle', ['lint-src', 'lint-test'],   bundle);
 
 gulp.task('test-browser', ['browser-bundle'], browserWatch);
+
+gulp.task('test-cross-browser', ['browser-bundle'], sauceRunner);

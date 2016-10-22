121d047e99237c7de2556cd3b37492cfe75791df
Roman Romanov <rroman4work@gmail.com>
2016-10-23 01:01:57 +0300

    Add "npm run esfix" command

--- gulp/linting.js
+++ gulp/linting.js
@@ -3,6 +3,7 @@ import eslint from 'gulp-eslint';
 import lintspaces from 'gulp-lintspaces';
 import plumber from 'gulp-plumber';
 import util from 'gulp-util';
+import gulpIf from 'gulp-if';
 
 function onError() {
   util.beep();
@@ -19,12 +20,31 @@ function lint(files) {
     .on('error', onError);
 }
 
+function isFixed(file) {
+	return file.eslint && file.eslint.fixed;
+}
+
+function lintFix(folder) {
+  return gulp.src(folder + '/**/*.js')
+    .pipe(eslint({ fix: true }))
+    .pipe(eslint.format())
+    .pipe(gulpIf(isFixed, gulp.dest(folder)));
+}
+
 function lintSrc() {
   return lint(['src/**/*.js']);
 }
 
+function lintSrcFix() {
+  return lintFix('src');
+}
+
 function lintTest() {
   return lint(['test/**/*.js']);
+}
+
+function lintTestFix() {
+  return lintFix('test');
 }
 
 function lintMd() {
@@ -37,10 +57,16 @@ function lintMd() {
 
 gulp.task('lint-src', lintSrc);
 
+gulp.task('lint-src-fix', lintSrcFix);
+
 gulp.task('lint-test', lintTest);
+
+gulp.task('lint-test-fix', lintTestFix);
 
 gulp.task('lint-code', ['lint-src', 'lint-test']);
 
 gulp.task('lint-docs', lintMd);
 
 gulp.task('lint', ['lint-code', 'lint-docs']);
+
+gulp.task('esfix', ['lint-src-fix', 'lint-test-fix']);

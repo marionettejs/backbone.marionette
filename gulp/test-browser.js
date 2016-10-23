edcbeba9907ad3c898b7f949795b48c094a1391e
denar90 <denysov.artem@gmail.com>
2016-10-23 17:29:22 +0300

    revert livereload removing, update easy-sauce version

--- gulp/test-browser.js
+++ gulp/test-browser.js
@@ -1,4 +1,5 @@
 import gulp from 'gulp';
+import livereload from 'gulp-livereload';
 import easySauce from 'easy-sauce';
 
 import { rollup } from 'rollup';
@@ -14,7 +15,7 @@ const sauceConf = {
   name: 'Marionette.js',
   username: process.env.SAUCE_USERNAME,
   key: process.env.SAUCE_ACCESS_KEY,
-  tests: '/test/runner.html',
+  testPath: '/test/runner.html',
   port: '8080',
   framework: 'mocha',
   platforms: [
@@ -43,47 +44,47 @@ function bundle() {
         exclude: 'node_modules/**'
       })
     ]
-  }).then(function (bundle) {
+  }).then(bundle => {
     return bundle.write({
       format: 'iife',
       sourceMap: true,
       moduleName: 'MnTests',
       dest: './tmp/__spec-build.js'
-    });
+    }).then(livereload.reload('./tmp/__spec-build.js'));
   });
 }
 
 function browserWatch() {
+  livereload.listen({port: 35729, host: 'localhost', start: true});
   opn('./test/runner.html');
   gulp.watch(['src/**/*.js', 'test/**/*.js'], ['browser-bundle']);
 }
 
 function sauceRunner() {
   easySauce(sauceConf)
-    .on('message', function(message) {
+    .on('message', message => {
       // A message has been emitted, inform the user.
       console.log(message);
     })
-    .on('update', function(job) {
+    .on('update', job => {
       // A job's status has been updated
       console.log(job.status);
     })
-    .on('done', function(passed, jobs) {
+    .on('done', (passed, jobs) => {
       // All tests have completed!
       if (passed) {
         console.log('All tests passed!');
-      }
-      else {
+      } else {
         console.log('Oops, there were failures:\n' + jobs);
       }
     })
-    .on('error', function(err) {
+    .on('error', error => {
       // An error occurred at some point running the tests.
-      throw new Error(err.message);
+      throw new Error(error.message);
     });
 }
 
-gulp.task('browser-bundle', ['lint-src', 'lint-test'],   bundle);
+gulp.task('browser-bundle', ['lint-src', 'lint-test'], bundle);
 
 gulp.task('test-browser', ['browser-bundle'], browserWatch);
 

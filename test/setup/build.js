58054b66293a79e963b89f22002a957d8be57373
denar90 <denysov.artem@gmail.com>
2016-12-15 23:58:23 +0200

    remove build tests

--- test/setup/build.js
+++
@@ -1,9 +0,0 @@
-require('./_node-env');
-var Backbone = require('backbone');
-
-require('babel-register');
-require('./setup')();
-
-require('../../lib/backbone.marionette.min');
-
-global.Marionette = Backbone.Marionette;

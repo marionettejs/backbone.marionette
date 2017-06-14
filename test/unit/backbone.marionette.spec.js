51b29e183bd8e1f836441b9c97a898bf17c6d776
Paul Falgout <paul@otterball.com>
2017-06-15 03:24:08 +0900

    Update docs and specs for DomApi

--- test/unit/backbone.marionette.spec.js
+++ test/unit/backbone.marionette.spec.js
@@ -1,3 +1,12 @@
+import _ from 'underscore';
+import Backbone from 'backbone';
+import Marionette from '../../src/backbone.marionette';
+import CollectionView from '../../src/collection-view';
+import CompositeView from '../../src/composite-view';
+import NextCollectionView from '../../src/next-collection-view';
+import Region from '../../src/region';
+import View from '../../src/view';
+
 describe('backbone.marionette', function() {
   'use strict';
 
@@ -11,6 +20,28 @@ describe('backbone.marionette', function() {
     it('should have a working getOption method which just returns when no optionName is passed', function() {
       const result = Marionette.getOption();
       expect(result).to.be.equal(undefined);
+    });
+  });
+
+  describe('#setDomApi', function() {
+    const DomClasses = {
+      CollectionView,
+      CompositeView,
+      NextCollectionView,
+      Region,
+      View
+    };
+
+    const fakeDomApi = {
+      foo: 'bar'
+    };
+
+    _.each(DomClasses, function(Class, key) {
+      it(`should setDomApi on ${ key }`, function() {
+        this.sinon.spy(Class, 'setDomApi');
+        Marionette.setDomApi(fakeDomApi);
+        expect(Class.setDomApi).to.be.calledOnce.and.calledWith(fakeDomApi);
+      });
     });
   });
 });

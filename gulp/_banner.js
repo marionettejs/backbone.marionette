1962fbb0066a2c0a1eecf4d62109548d02483c64
Luiz Américo <camara_luiz@yahoo.com.br>
2017-08-10 13:11:31 -0300

    Use source map generated by rollup directly instead of loading through gulp-sourcemaps (#3420)
    * Use source map generated by rollup directly instead of loading through gulp-sourcemaps
    
    * Configure build to preserve license comment on minified source

--- gulp/_banner.js
+++ gulp/_banner.js
@@ -3,11 +3,14 @@ import {version} from '../package.json';
 const now = new Date();
 const year = now.getFullYear();
 
-export default `// MarionetteJS (Backbone.Marionette)
-// ----------------------------------
-// v${version}
-//
-// Copyright (c)${year} Derick Bailey, Muted Solutions, LLC.
-// Distributed under MIT license
-//
-// http://marionettejs.com\n\n`;
+export default `/**
+* @license
+* MarionetteJS (Backbone.Marionette)
+* ----------------------------------
+* v${version}
+*
+* Copyright (c)${year} Derick Bailey, Muted Solutions, LLC.
+* Distributed under MIT license
+*
+* http://marionettejs.com
+*/\n\n`;

d0d414498a4b87ce7261e687d6aac354d23e87d5
Paul Falgout <paul@otterball.com>
2017-11-01 01:19:42 +0900

    Replace viewFilter _.partition (#3512)
    * Remove viewFilter partition
    * Move context binding to child view container

--- src/next-collection-view.js
+++ src/next-collection-view.js
@@ -370,12 +370,7 @@ const CollectionView = Backbone.View.extend({
 
     this.triggerMethod('before:sort', this);
 
-    if (_.isFunction(viewComparator)) {
-      // Must use native bind to preserve length
-      viewComparator = viewComparator.bind(this);
-    }
-
-    this.children._sort(viewComparator);
+    this.children._sort(viewComparator, this);
 
     this.triggerMethod('sort', this);
   },
@@ -453,13 +448,18 @@ const CollectionView = Backbone.View.extend({
 
     this.triggerMethod('before:filter', this);
 
-    const filteredViews = _.partition(this.children._views, _.bind(viewFilter, this));
+    const attachViews = [];
+    const detachViews = [];
 
-    this._detachChildren(filteredViews[1]);
+    _.each(this.children._views, (view, key, children) => {
+      (viewFilter.call(this, view, key, children) ? attachViews : detachViews).push(view);
+    });
 
-    this.triggerMethod('filter', this);
+    this._detachChildren(detachViews);
 
-    return filteredViews[0];
+    this.triggerMethod('filter', this, attachViews, detachViews);
+
+    return attachViews;
   },
 
   // This method returns a function for the viewFilter

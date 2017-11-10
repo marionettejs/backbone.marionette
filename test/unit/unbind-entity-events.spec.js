01618730364e6dd02c955a1a32843d122cfbcf18
wesmangum <wesmangum24@gmail.com>
2017-11-10 09:19:53 -0600

    refactor and reorg templateContext and unbind unit tests

--- test/unit/unbind-entity-events.spec.js
+++ test/unit/unbind-entity-events.spec.js
@@ -1,81 +1,89 @@
-describe('Marionette.unbindEntityEvents', function() {
+import { unbindEvents } from '../../src/backbone.marionette';
+
+describe('unbindEntityEvents', function() {
   'use strict';
 
-  beforeEach(function() {
-    this.fooStub = this.sinon.stub();
-    this.barStub = this.sinon.stub();
-    this.stopListeningStub = this.sinon.stub();
+  let fooStub;
+  let barStub;
+  let stopListeningStub;
+  let target;
+  let entity;
 
-    this.target = {
-      foo: this.fooStub,
-      bar: this.barStub,
-      stopListening: this.stopListeningStub
+  beforeEach(function() {
+    fooStub = this.sinon.stub();
+    barStub = this.sinon.stub();
+    stopListeningStub = this.sinon.stub();
+
+    target = {
+      foo: fooStub,
+      bar: barStub,
+      stopListening: stopListeningStub
     };
 
-    this.entity = this.sinon.spy();
+    entity = this.sinon.spy();
   });
 
   describe('when entity isnt passed', function() {
     beforeEach(function() {
-      Marionette.unbindEvents(this.target, false, {'foo': 'foo'});
+      unbindEvents(target, false, {'foo': 'foo'});
     });
 
     it('shouldnt unbind any events', function() {
-      expect(this.stopListeningStub).not.to.have.been.called;
+      expect(stopListeningStub).not.to.have.been.called;
     });
 
     it('should return the target', function() {
-      expect(Marionette.unbindEvents(this.target, false, {'foo': 'foo'})).to.equal(this.target);
+      expect(unbindEvents(target, false, {'foo': 'foo'})).to.equal(target);
     });
   });
 
   describe('when bindings isnt passed', function() {
     beforeEach(function() {
-      Marionette.unbindEvents(this.target, this.entity, null);
+      unbindEvents(target, entity, null);
     });
 
     it('should unbind all events', function() {
-      expect(this.stopListeningStub).to.have.been.calledOnce.and.calledWith(this.entity);
+      expect(stopListeningStub).to.have.been.calledOnce.and.calledWith(entity);
     });
 
     it('should return the target', function() {
-      expect(Marionette.unbindEvents(this.target, this.entity, null)).to.equal(this.target);
+      expect(unbindEvents(target, entity, null)).to.equal(target);
     });
   });
 
   describe('when bindings is an object with one event-handler pair', function() {
     describe('when handler is a function', function() {
       beforeEach(function() {
-        Marionette.unbindEvents(this.target, this.entity, {'foo': this.target.foo});
+        unbindEvents(target, entity, {'foo': target.foo});
       });
 
       it('should unbind an event', function() {
-        expect(this.stopListeningStub).to.have.been.calledOnce.and.calledWith(this.entity, 'foo', this.target.foo);
+        expect(stopListeningStub).to.have.been.calledOnce.and.calledWith(entity, 'foo', target.foo);
       });
     });
 
     describe('when handler is a string', function() {
       describe('when one handler is passed', function() {
         beforeEach(function() {
-          Marionette.unbindEvents(this.target, this.entity, {'foo': 'foo'});
+          unbindEvents(target, entity, {'foo': 'foo'});
         });
 
         it('should unbind an event', function() {
-          expect(this.stopListeningStub).to.have.been.calledOnce.and.calledWith(this.entity, 'foo', this.target.foo);
+          expect(stopListeningStub).to.have.been.calledOnce.and.calledWith(entity, 'foo', target.foo);
         });
       });
 
       describe('when multiple handlers are passed', function() {
         beforeEach(function() {
-          Marionette.unbindEvents(this.target, this.entity, {'baz': 'foo bar'});
+          unbindEvents(target, entity, {'baz': 'foo bar'});
         });
 
         it('should unbind first event', function() {
-          expect(this.stopListeningStub).to.have.been.calledWith(this.entity, 'baz', this.target.foo);
+          expect(stopListeningStub).to.have.been.calledWith(entity, 'baz', target.foo);
         });
 
         it('should unbind second event', function() {
-          expect(this.stopListeningStub).to.have.been.calledWith(this.entity, 'baz', this.target.bar);
+          expect(stopListeningStub).to.have.been.calledWith(entity, 'baz', target.bar);
         });
       });
     });
@@ -83,18 +91,18 @@ describe('Marionette.unbindEntityEvents', function() {
 
   describe('when bindings is an object with multiple event-handler pairs', function() {
     beforeEach(function() {
-      Marionette.unbindEvents(this.target, this.entity, {
+      unbindEvents(target, entity, {
         'foo': 'foo',
         'bar': 'bar'
       });
     });
 
     it('should unbind first event', function() {
-      expect(this.stopListeningStub).to.have.been.calledWith(this.entity, 'foo', this.target.foo);
+      expect(stopListeningStub).to.have.been.calledWith(entity, 'foo', target.foo);
     });
 
     it('should unbind second event', function() {
-      expect(this.stopListeningStub).to.have.been.calledWith(this.entity, 'bar', this.target.bar);
+      expect(stopListeningStub).to.have.been.calledWith(entity, 'bar', target.bar);
     });
   });
 });

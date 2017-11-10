47c9a210cec2fe926c94e527cfed83b0f73608b7
wesmangum <wesmangum24@gmail.com>
2017-11-10 09:19:53 -0600

    refactor and reorg templateContext and unbind unit tests

--- test/unit/template-context.spec.js
+++ test/unit/template-context.spec.js
@@ -1,101 +1,121 @@
+import View from '../../src/view';
+
 describe('template context methods', function() {
   'use strict';
 
+  let templateStub;
+  let templateContext;
+  let templateContextFn;
+  let modelData;
+  let model;
+
   beforeEach(function() {
-    this.templateStub = this.sinon.stub();
-    this.templateContext = {foo: this.sinon.stub()};
-    this.templateContextFn = this.sinon.stub().returns(this.templateContext);
-    this.modelData = {bar: 'baz'};
-    this.model = new Backbone.Model(this.modelData);
+    templateStub = this.sinon.stub();
+    templateContext = {foo: this.sinon.stub()};
+    templateContextFn = this.sinon.stub().returns(templateContext);
+    modelData = {bar: 'baz'};
+    model = new Backbone.Model(modelData);
   });
 
   describe('view', function() {
     describe('when rendering with no model or collection and a templateContext is found', function() {
+      let TestView;
+      let view;
+
       beforeEach(function() {
-        this.View = Marionette.View.extend({
-          templateContext: this.templateContext,
-          template: this.templateStub
+        TestView = View.extend({
+          templateContext: templateContext,
+          template: templateStub
         });
 
-        this.view = new this.View();
-        this.view.render();
+        view = new TestView();
+        view.render();
       });
 
       it('should include the template context in the data object', function() {
-        expect(this.templateStub).to.have.been.calledOnce.and.calledWith(this.templateContext);
+        expect(templateStub).to.have.been.calledOnce.and.calledWith(templateContext);
       });
     });
 
     describe('when rendering with a model, and a templateContext is found', function() {
+      let TestView;
+      let view;
+
       beforeEach(function() {
-        this.View = Marionette.View.extend({
-          templateContext: this.templateContext,
-          template: this.templateStub
+        TestView = View.extend({
+          templateContext: templateContext,
+          template: templateStub
         });
 
-        this.view = new this.View({
-          model: this.model
+        view = new TestView({
+          model: model
         });
 
-        this.view.render();
+        view.render();
       });
 
       it('should include the template context in the data object', function() {
-        expect(this.templateStub.lastCall.args[0]).to.contain(this.templateContext);
+        expect(templateStub.lastCall.args[0]).to.contain(templateContext);
       });
 
       it('should still have the data from the model', function() {
-        expect(this.templateStub.lastCall.args[0]).to.contain(this.modelData);
+        expect(templateStub.lastCall.args[0]).to.contain(modelData);
       });
     });
 
     describe('when rendering and a templateContext is found as a function', function() {
+      let TestView;
+      let view;
+
       beforeEach(function() {
-        this.View = Marionette.View.extend({
-          templateContext: this.templateContextFn,
-          template: this.templateStub
+        TestView = View.extend({
+          templateContext: templateContextFn,
+          template: templateStub
         });
 
-        this.view = new this.View({
-          model: this.model
+        view = new TestView({
+          model: model
         });
 
-        this.view.render();
+        view.render();
       });
 
       it('should include the template context in the data object', function() {
-        expect(this.templateStub.lastCall.args[0]).to.contain(this.templateContext);
+        expect(templateStub.lastCall.args[0]).to.contain(templateContext);
       });
 
       it('should still have the data from the model', function() {
-        expect(this.templateStub.lastCall.args[0]).to.contain(this.modelData);
+        expect(templateStub.lastCall.args[0]).to.contain(modelData);
       });
 
       it('should maintain the view as the context for the templateContext function', function() {
-        expect(this.templateContextFn).to.have.been.calledOnce.and.calledOn(this.view);
+        expect(templateContextFn).to.have.been.calledOnce.and.calledOn(view);
       });
     });
 
     describe('when templateContext is provided to constructor options', function() {
+      let TestView;
+      let view;
+
       beforeEach(function() {
-        this.View = Marionette.View.extend({
-          template: this.templateStub
+        TestView = View.extend({
+          template: templateStub
         });
 
-        this.view = new this.View({
-          templateContext: this.templateContext,
-          model: this.model
+        view = new TestView({
+          templateContext: templateContext,
+          model: model
         });
 
-        this.view.render();
+        view.render();
       });
 
       it('should include the template context in the data object', function() {
-        expect(this.templateStub.lastCall.args[0]).to.contain(this.templateContext);
+        expect(templateStub.lastCall.args[0]).to.contain(templateContext);
       });
 
       it('should still have the data from the model', function() {
-        expect(this.templateStub.lastCall.args[0]).to.contain(this.modelData);
+        expect(templateStub.lastCall.args[0]).to.contain(modelData);
       });
     });
   });
--- test/unit/unbind-radio-requests.spec.js
+++ test/unit/unbind-radio-requests.spec.js
@@ -1,68 +1,76 @@
-describe('Marionette.unbindRequests', function() {
+import { unbindRequests } from '../../src/backbone.marionette';
+
+describe('unbindRequests', function() {
   'use strict';
 
-  beforeEach(function() {
-    this.replyFooStub = this.sinon.stub();
-    this.replyBarStub = this.sinon.stub();
-    this.stopReplyingStub = this.sinon.stub();
+  let replyFooStub;
+  let replyBarStub;
+  let stopReplyingStub;
+  let target;
+  let channel;
 
-    this.target = {
-      replyFoo: this.replyFooStub,
-      replyBar: this.replyBarStub
+  beforeEach(function() {
+    replyFooStub = this.sinon.stub();
+    replyBarStub = this.sinon.stub();
+    stopReplyingStub = this.sinon.stub();
+
+    target = {
+      replyFoo: replyFooStub,
+      replyBar: replyBarStub
     };
 
-    this.channel = {
-      stopReplying: this.stopReplyingStub
+    channel = {
+      stopReplying: stopReplyingStub
     };
   });
 
   describe('when channel isnt passed', function() {
     beforeEach(function() {
-      Marionette.unbindRequests(this.target, false, {'foo': 'foo'});
+      unbindRequests(target, false, {'foo': 'foo'});
     });
 
     it('shouldnt unbind any request', function() {
-      expect(this.stopReplyingStub).not.to.have.been.called;
+      expect(stopReplyingStub).not.to.have.been.called;
     });
 
     it('should return the target', function() {
-      expect(Marionette.unbindRequests(this.target, false, {'foo': 'foo'})).to.equal(this.target);
+      expect(unbindRequests(target, false, {'foo': 'foo'})).to.equal(target);
     });
   });
 
   describe('when bindings isnt passed', function() {
     beforeEach(function() {
-      Marionette.unbindRequests(this.target, this.channel, null);
+      unbindRequests(target, channel, null);
     });
 
     it('should unbind all requests', function() {
-      expect(this.stopReplyingStub).to.have.been.calledOnce.and.calledWith(null, null, this.target);
+      expect(stopReplyingStub).to.have.been.calledOnce.and.calledWith(null, null, target);
     });
 
     it('should return the target', function() {
-      expect(Marionette.unbindRequests(this.target, this.channel, null)).to.equal(this.target);
+      expect(unbindRequests(target, channel, null)).to.equal(target);
     });
   });
 
   describe('when bindings is an object with one request-handler pair', function() {
     describe('when handler is a function', function() {
       beforeEach(function() {
-        Marionette.unbindRequests(this.target, this.channel, {'foo': this.target.replyFoo});
+        unbindRequests(target, channel, {'foo': target.replyFoo});
       });
 
       it('should unbind an request', function() {
-        expect(this.stopReplyingStub).to.have.been.calledOnce.and.calledWith({'foo': this.target.replyFoo});
+        expect(stopReplyingStub).to.have.been.calledOnce.and.calledWith({'foo': target.replyFoo});
       });
     });
 
     describe('when handler is a string', function() {
       describe('when one handler is passed', function() {
         beforeEach(function() {
-          Marionette.unbindRequests(this.target, this.channel, {'foo': 'replyFoo'});
+          unbindRequests(target, channel, {'foo': 'replyFoo'});
         });
 
         it('should unbind an request', function() {
-          expect(this.stopReplyingStub).to.have.been.calledOnce.and.calledWith({'foo': this.target.replyFoo});
+          expect(stopReplyingStub).to.have.been.calledOnce.and.calledWith({'foo': target.replyFoo});
         });
       });
     });
@@ -70,14 +78,14 @@ describe('Marionette.unbindRequests', function() {
 
   describe('when bindings is an object with multiple request-handler pairs', function() {
     beforeEach(function() {
-      Marionette.unbindRequests(this.target, this.channel, {
+      unbindRequests(target, channel, {
         'foo': 'replyFoo',
         'bar': 'replyBar'
       });
     });
 
     it('should unbind first request', function() {
-      expect(this.stopReplyingStub).to.have.been.calledWith({'bar': this.target.replyBar, 'foo': this.target.replyFoo});
+      expect(stopReplyingStub).to.have.been.calledWith({'bar': target.replyBar, 'foo': target.replyFoo});
     });
   });
 });

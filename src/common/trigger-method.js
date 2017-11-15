d8ed73fa55410795a931eff1ccb0cefa0786c303
Luiz Américo <luizamericop@gmail.com>
2017-11-15 06:21:13 -0300

    Create rest arguments in triggerMethod only when there's a method

--- src/common/trigger-method.js
+++ src/common/trigger-method.js
@@ -24,7 +24,7 @@ const getOnMethodName = _.memoize(function(event) {
 //
 // `this.triggerMethod("foo:bar")` will trigger the "foo:bar" event and
 // call the "onFooBar" method.
-export default function triggerMethod(event, ...args) {
+export default function triggerMethod(event) {
   // get the method name from the event name
   const methodName = getOnMethodName(event);
   const method = getOption.call(this, methodName);
@@ -33,7 +33,7 @@ export default function triggerMethod(event, ...args) {
   // call the onMethodName if it exists
   if (_.isFunction(method)) {
     // pass all args, except the event name
-    result = method.apply(this, args);
+    result = method.apply(this, _.rest(arguments));
   }
 
   // trigger the event
